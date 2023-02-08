214b7936bcbbd959caeb4cea5db5ba0b97fea57b
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2023-02-08 20:35:01 +0300

    fix

--- zdn/misc_independent_work_der/misc_independent_work_der.js
+++ zdn/misc_independent_work_der/misc_independent_work_der.js
@@ -2,7 +2,7 @@ if(!window.nabor)
 	window.nabor={};
 window.nabor.importFrom({
 	nZad:2,
-	adres:'../misc_independent_work_der/',
+	adres:'../zdn/misc_independent_work_der/',
 	name:'misc_independent_work_der',
 	prefix:'',
 });
