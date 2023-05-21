47d5243a3ee4b8ca4b51142a0e80b5928b94fb47
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2023-05-21 11:50:27 +0300

    [new] [misc] - misc_cylinder

---
+++ zdn/misc_cylinder/1/main.js
@@ -0,0 +1,7 @@
+window.nomer=[
+	27045,
+	27046,
+	27058,
+	27118,
+].iz()
+window.comment='';
---
+++ zdn/misc_cylinder/misc_cylinder.js
@@ -0,0 +1,8 @@
+if(!window.nabor)
+	window.nabor={};
+window.nabor.importFrom({
+	nZad:1,
+	adres:'../zdn/misc_cylinder/',
+	name:'misc_cylinder',
+	prefix:'',
+});
