e58cd8386e5d24a8b70c8bda9129715ec0c30e08
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2023-05-29 21:11:11 +0300

    new main

---
+++ zdn/work_stereometry/1/main.js
@@ -0,0 +1,7 @@
+window.nomer=[
+	3,
+	4,
+	9,
+	27130,
+].iz()
+window.comment='Куб';
---
+++ zdn/work_stereometry/work_stereometry.js
@@ -0,0 +1,7 @@
+if(!window.nabor)
+	window.nabor={};
+window.nabor.importFrom({
+	nZad:5,
+	adres:'../zdn/work_stereometry/',
+	name:'work_stereometry',
+});
