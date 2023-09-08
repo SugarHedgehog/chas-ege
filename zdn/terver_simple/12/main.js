8db7d6043d15754d637216c5acff7d20821d85cc
SugarHedgehog <sneginca.cat@gmail.com>
2023-09-08 13:51:13 +0300

    test

---
+++ zdn/terver_simple/12/main.js
@@ -0,0 +1,4 @@
+window.nomer=[
+	320190,
+].iz()
+window.comment='320190';
---
+++ zdn/terver_simple/terver_simple.js
@@ -0,0 +1,7 @@
+if (!window.nabor)
+	window.nabor = {};
+window.nabor.importFrom({
+	nZad: 15,
+ 	adres: '../zdn/terver_simple',
+	name: 'terver_simple',
+});
