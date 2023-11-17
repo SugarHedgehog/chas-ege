cd57cac89760c5a29538f9ae7e32134574fac565
SugarHedgehog <sneginca.cat@gmail.com>
2023-11-17 15:27:40 +0300

    [misc] [new] - Tasks with applied content

---
+++ zdn/misc_tasks_with_applied_content_log/misc_tasks_with_applied_content_log.js
@@ -0,0 +1,7 @@
+if (!window.nabor)
+	window.nabor = {};
+window.nabor.importFrom({
+	nZad: 3,
+ 	adres: '../zdn/misc_tasks_with_applied_content_log/',
+	name: 'misc_tasks_with_applied_content_log',
+});
