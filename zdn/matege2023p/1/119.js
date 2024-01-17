867bb2caa3d284e71c74a4f9bea6470425101b8d
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2024-01-17 21:25:33 +0300

    [zdn] [new] - newTask_119&121

--- zdn/matege2023p/1/119.js
+++ zdn/matege2023p/1/119.js
@@ -23,7 +23,7 @@
 
 	let paint1 = function(ctx) {
 		ctx.lineWidth = 2;
-		ctx.strokeStyle = "#809DF2";
+		ctx.strokeStyle = om.secondaryBrandColors.iz();
 		ctx.beginPath();
 		ctx.arc(200, 200, 180, 0, 2 * Math.PI);
 		ctx.stroke();
@@ -32,7 +32,7 @@
 		let deltaY = (angle > 90 ? 165 : 111);
 
 		//центральный
-		ctx.strokeStyle = ["#D777F2","#F2A2D6"].iz();
+		ctx.strokeStyle = om.primaryBrandColors.iz();
 		ctx.drawFilledCircle(200, 200, 3);
 		ctx.drawLine(200 - deltaX, 200 - deltaY, 200 + deltaX, 200 + deltaY);
 		ctx.drawLine(200 - deltaX, 200 + deltaY, 200 + deltaX, 200 - deltaY);
