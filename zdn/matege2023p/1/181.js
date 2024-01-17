bbca5dc25daf84dad433a95efb89a2aeb2f26fe2
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2024-01-17 21:32:11 +0300

    merge "newTask_181"

--- zdn/matege2023p/1/181.js
+++ zdn/matege2023p/1/181.js
@@ -20,13 +20,13 @@
 
 		let paint1 = function(ctx) {
 			ctx.lineWidth = 2;
-			ctx.strokeStyle = "#809DF2";
+			ctx.strokeStyle = om.secondaryBrandColors.iz();
 
 			ctx.beginPath();
 			ctx.arc(200, 200, 180, 0, 2 * Math.PI);
 			ctx.stroke();
 			
-			ctx.strokeStyle = ["#D777F2","#F2A2D6"].iz();
+			ctx.strokeStyle = om.primaryBrandColors.iz();
 
 			ctx.drawLine(200 - 160, 200 - 86, 200 - 120, 200 + 134);
 			ctx.drawLine(200 - 120, 200 + 134, 200 + 82, 200 + 160);
