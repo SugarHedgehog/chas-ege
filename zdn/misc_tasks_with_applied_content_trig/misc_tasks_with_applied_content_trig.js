4365c6e9de121d8697dc325898ce3d61a0479c4b
SugarHedgehog <sneginca.cat@gmail.com>
2024-02-06 18:44:17 +0300

    misc_tasks_with_applied_content_trig

---
+++ zdn/misc_tasks_with_applied_content_trig/misc_tasks_with_applied_content_trig.js
@@ -0,0 +1,7 @@
+if (!window.nabor)
+	window.nabor = {};
+window.nabor.importFrom({
+	nZad: 4,
+ 	adres: '../zdn/misc_tasks_with_applied_content_trig/',
+	name: 'misc_tasks_with_applied_content_trig',
+});
