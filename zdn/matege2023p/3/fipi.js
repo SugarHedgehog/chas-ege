f84273eb276601a3756b4e0c8d0ed0f9d15f1f5f
SugarHedgehog <sneginca.cat@gmail.com>
2024-02-24 20:57:25 +0300

    [var] [14] - option

--- zdn/matege2023p/3/fipi.js
+++ zdn/matege2023p/3/fipi.js
@@ -1,17 +1,4 @@
 window.availableTaskNumbers = [
-     1,
-     // 3,
-     // 4,
-     // 5,
-     // 6,
-     // 7,
-     8,
-     // 9,
-     // 10,
-     // 12,
-     // 320190,
-     // 320191,
-     // 320194,
      320195,
 ];
 window.nomer = window.availableTaskNumbers.iz();
--- zdn/matege2023p/7/fipi.js
+++ zdn/matege2023p/7/fipi.js
@@ -1,17 +1,5 @@
 window.availableTaskNumbers = [
-      // 1,
-      // 2,
-      // 3,
-      // 4,
-      5,
-      // 27489,
-      // 27501,
-      // 27505,
       119979,
-      // 525690,
-      // 562751,
-      // 40130,
-      // 510384,
 ];
 window.nomer = window.availableTaskNumbers.iz();
 window.comment = 'Производная и первообразная';
