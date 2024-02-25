90687874a53c9b3a6fb3fe8c39b1dfe174302bb9
SugarHedgehog <sneginca.cat@gmail.com>
2024-02-25 12:06:13 +0300

    [var] [15] - delete task

--- zdn/matege2024p/4/fipi.js
+++ zdn/matege2024p/4/fipi.js
@@ -1,1 +1,11 @@
-../../matege2023p/3/fipi.js
+window.availableTaskNumbers = [
+     // 3,
+     // 4,
+     // 5,
+     // 6,
+     // 7,
+     8,
+     // 9,
+];
+window.nomer = window.availableTaskNumbers.iz();
+window.comment = 'Начала теории вероятностей';
--- zdn/matege2024p/9/fipi.js
+++ zdn/matege2024p/9/fipi.js
@@ -1,1 +1,22 @@
-../../matege2023p/8/fipi.js
+window.availableTaskNumbers = [
+    // 1,
+    // 2,
+    3,
+    // 4,
+    // 5,
+    // 6,
+    // 7,
+    8,
+    // 9,
+    // 10,
+    // 11,
+    // 12,
+    // 27965,
+    // 27984,
+    27996,
+    28003,
+    28009,
+    28012,
+];
+window.nomer = window.availableTaskNumbers.iz();
+window.comment = 'Задачи с прикладным содержанием';
