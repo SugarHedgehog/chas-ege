11ca28095d65817703d4b7b20c5ce021b4e4f92c
SugarHedgehog <sneginca.cat@gmail.com>
2024-03-02 18:18:37 +0300

    [zdn] [new] - 275020001

---
+++ zdn/misc_research_functions_derivative_with_image/misc_research_functions_derivative_with_image.js
@@ -0,0 +1,8 @@
+if(!window.nabor)
+	window.nabor={};
+window.nabor.importFrom({
+	nZad:12,
+	adres:'../zdn/misc_research_functions_derivative_with_image/',
+	name:'misc_research_functions_derivative_with_image',
+	prefix:'',
+});
