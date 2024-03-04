0e43a66a36a209d6124eb499efac370eebd3342f
SugarHedgehog <sneginca.cat@gmail.com>
2024-03-04 12:48:30 +0300

    test

--- zdn/misc_research_functions_derivative_with_image/misc_research_functions_derivative_with_image.js
+++ zdn/misc_research_functions_derivative_with_image/misc_research_functions_derivative_with_image.js
@@ -1,11 +1,3 @@
-if(!window.nabor)
-	window.nabor={};
-window.nabor.importFrom({
-	nZad:12,
-	adres:'../zdn/misc_research_functions_derivative_with_image/',
-	name:'misc_research_functions_derivative_with_image',
-	prefix:'',
-});
 if (!window.nabor)
 	window.nabor = {};
 window.nabor.importFrom({
