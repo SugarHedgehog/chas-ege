66cac14ff6ca1ffc0224d17aa5503f62f6102947
SugarHedgehog <sneginca.cat@gmail.com>
2024-09-05 22:05:47 +0400

    misc_tasks_with_applied_content_2025

---
+++ zdn/misc_task_with_applied_content_fipi_2025/misc_task_with_applied_content_fipi_2025.js
@@ -0,0 +1,7 @@
+if (!window.nabor)
+	window.nabor = {};
+window.nabor.importFrom({
+	nZad: 32,
+ 	adres: '../zdn/misc_task_with_applied_content_fipi_2025/',
+	name: 'misc_task_with_applied_content_fipi_2025',
+});
