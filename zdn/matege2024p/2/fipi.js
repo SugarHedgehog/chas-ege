c1593a0313614691b971e54dc6b06fbf40c3547a
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2024-10-20 13:51:39 +0500

    var_2425_18

--- zdn/matege2024p/2/fipi.js
+++ zdn/matege2024p/2/fipi.js
@@ -1,8 +1,5 @@
-window.nomer=[
-	4504,
-	644849,
-	644850,
+window.availableTaskNumbers = [
 	649891,
-	64485008,
-].iz()
-window.comment='Вектора';
+];
+window.nomer = window.availableTaskNumbers.iz();
+window.comment='649891';
