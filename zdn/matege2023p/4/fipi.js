2e7cd07d65ab32d89bf77e89f983c797728635fe
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2024-10-20 13:26:41 +0500

    var_2425_9

--- zdn/matege2023p/4/fipi.js
+++ zdn/matege2023p/4/fipi.js
@@ -1,15 +1,5 @@
-window.nomer = [
-    2,
-    13,
-    14,
-    320196,
-    320197,
-    320198,
-    320200,
-    320201,
-    320202,
-    320203,
-    523366,
-    621772,
-].iz();
-window.comment = 'Вероятности сложных событий';
+window.availableTaskNumbers = [
+	320188,
+];
+window.nomer = window.availableTaskNumbers.iz();
+window.comment='320188';
