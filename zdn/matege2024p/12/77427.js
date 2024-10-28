e3e6d3faeab94d3f875fae68969d8d4b12bd0417
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2024-10-28 14:38:35 +0500

    cut 77427

--- zdn/matege2024p/12/77427.js
+++ zdn/matege2024p/12/77427.js
@@ -1,14 +1,39 @@
-(function() {
-	retryWhileError(function() {
+(function () {
+	retryWhileError(function () {
 		'use strict';
+		let linear = [sl(1, 100) + 'x ', ''];
+		let forbidMinY = false;
+		let forbidMaxY = false;
+
+		console.log(nabor.preferences["77427"]);
+		if (nabor.preferences && "77427" in nabor.preferences) {
+			switch (nabor.preferences["77427"][0]) {
+				case 'linear':
+					linear = [['+', '-'].iz() + sl(1, 100) + 'x '];
+					break;
+				case 'not_linear':
+					linear = [''];
+					break;
+			}
+			switch (nabor.preferences["77427"][1]) {
+				case 'minimum':
+					forbidMaxY = true;
+					break;
+				case 'maximum':
+					forbidMinY = true;
+					break;
+			}
+		}
+
 		NAtask.setLocalExtremumTask({
 			expr:
 				// TODO: Do something with 1x
 				'' /*+ ['',sl(2,4).pm()].iz()*/ + 'x^3 ' +
-				['+','-'].iz() + sl(1,100) + 'x^2 ' +
-				['+','-'].iz() + sl(1,100) + 'x ' +
-				['+','-'].iz() + sl(1,1000),
+				['+', '-'].iz() + sl(1, 100) + 'x^2 ' + linear.iz() +
+				['+', '-'].iz() + sl(1, 1000),
 			authors: ['Николай Авдеев'],
+			forbidMinY: forbidMinY,
+			forbidMaxY: forbidMaxY,
 		});
 	}, 2000);
 })();
