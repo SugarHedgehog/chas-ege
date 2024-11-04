1f4fdec640fc47f0f3278a6f012cad60d0503b86
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2024-11-04 12:28:00 +0500

    fix task

--- zdn/matege2022p/9/508895.js
+++ zdn/matege2022p/9/508895.js
@@ -1,5 +1,8 @@
 (function() {
 	NAinfo.requireApiVersion(0, 2);
+	function f(x) {
+		return k*x +b;
+	}
 	let k = [1, 2, 3, 0.2, 0.4, 0.5, ].iz().pm();
 	let b = sluchch(0, 3).pm();
 	let chisl = sluchch(7, 20, 0.5).pm();
@@ -7,18 +10,17 @@
 	let X = [],
 		Y = [];
 	for (let i = -6; i < 0; i++)
-		if ((k * i + b).isZ() && Math.abs(k * i + b) < 6) {
-			X.push(i * 20);
-			Y.push((k * i + b) * 20);
+		if (f(i).isZ() && Math.abs(f(i)) < 6) {
+			X.push(i);
+			Y.push(f(i));
 			//break;
 		}
 	for (let i = 6; i >= 0; i--)
-		if ((k * i + b).isZ() && Math.abs(k * i + b) < 6) {
-			X.push(i * 20);
-			Y.push((k * i + b) * 20);
+		if (f(i).isZ() && Math.abs(f(i) < 6)) {
+			X.push(i);
+			Y.push(f(i));
 			//break;
 		}
-	b *= 20;
 	let paint1 = function(ct) {
 		let h = 400;
 		let w = 400;
@@ -33,28 +35,30 @@
 		}, 20);
 		//график
 		
-		ct.scale(1, -1);
-		for (let i = -130; i < 130; i++)
-			if (Math.abs(k * (i - 1) + b) < 110)
-				ct.drawLine(i - 1, k * (i - 1) + b, i, k * i + b);
+		let scale = 20;
+		ct.scale(scale, -scale);
+		ct.lineWidth = 2/scale;
+		for (let i = -8.5; i < 8.5; i++)
+			if (Math.abs(f(i-1)) < 8.5)
+				ct.drawLine(i - 1, f(i-1), i, f(i));
 			//точки
-		graph9AmarkCircles(ct, [X,Y].T(), 2);
+		graph9AmarkCircles(ct, [X,Y].T(), 2, 3/scale);
 	};
 	NAtask.setTask({
 		text: `На рисунке изображён график функции $f(x)=kx+b$. Найдите `,
 		questions: [
 			{
 				text: `$f(${chisl.ts()})$`,
-				answer: (chisl * k + b).ts(),
+				answer: f(chisl),
 			},
 			{
-				text: `значение $x$, при котором $f(x)=${(chisl * k+ b).ts() }$`,
-				answer: chisl.ts(),
+				text: `значение $x$, при котором $f(x)=${f(chisl).ts() }$`,
+				answer: chisl,
 				analys: `, $x=\\frac{${chisl}-${b}}{${k}}$`.plusminus(),
 			},
 		],
 		postquestion: `.`,
-		analys: `$f(x)=` + (k + `x+` + (b / 20)).replace('+0', '').plusminus() + `$`,
+		analys: `$f(x)=` + (k + `x+` + (b)).replace('+0', '').plusminus() + `$`,
 	});
 	NAtask.modifiers.addCanvasIllustration({
 		width: 400,
