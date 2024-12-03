4f530fd32635700f00e685eeddb8440a3d65da47
SugarHedgehog <sneginca.cat@gmail.com>
2024-12-03 17:13:14 +0300

    comment for 6

--- zdn/misc_beginnings_of_probability_theory_2025/13/main.js
+++ zdn/misc_beginnings_of_probability_theory_2025/13/main.js
@@ -1,6 +1,6 @@
 window.nomer=[
 	6,
 ].iz()
-window.comment='6 в первые дни';
+window.comment='6 запланировано в первые дни';
 window.nabor.preferences['6'] = ['first_day'];
 chas2.task.setMinimaxFunctionTask.forbidOpenEnds = true;
