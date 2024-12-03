d44cdddcb002027ffd9f0569a26a48cf1acf7996
SugarHedgehog <sneginca.cat@gmail.com>
2024-12-03 17:03:11 +0300

    cut 320183

--- zdn/matege2023p/3/320183.js
+++ zdn/matege2023p/3/320183.js
@@ -8,23 +8,51 @@
 (function () {
     'use strict';
     NAinfo.requireApiVersion(0, 0);
+    let key = "320183";
 
     const sport = ['футбольного', 'волейбольного', 'баскетбольного', 'хоккейного', 'теннисного', 'бейсбольного'].iz();
     const team_name = ['Физик', 'Химик', 'Сапфир', 'Труд', 'Геолог', 'Биолог', 'Квант', 'Изумруд', 'Рубин', 'Факел'].iz();
     const vopr = ['начнёт игру с мячом', 'выиграет жребий'].iz();
     
     let kolvo_rush = sl(0, 3) + 2; // число сыгранных игр   0-2 игры 1-3 игры 2-4 игры 3-5 игр
-    const games = ['два матча', 'три матча', 'четыре матча', 'пять матчей'][kolvo_rush - 2];
-    const first_times = sl(1, kolvo_rush); // число выигранных жребиев
-    let rovno_ili_bol = sluchch(0, 1); // ровно %количество побед% = 0, больше = 1
-
-    if (kolvo_rush === first_times) {
-        rovno_ili_bol = 0;
+    kolvo_rush = getListedPreference(key, [{
+        preference: 'two_games',
+        preferenceValue: 2,
+    }, {
+        preference: 'three_games',
+        preferenceValue: 4,
+    }, {
+        preference: 'four_games',
+        preferenceValue: 4,
+    }, {
+        preference: 'five_games',
+        preferenceValue: 5,
     }
+    ], kolvo_rush);
+
+    const games = ['два матча', 'три матча', 'четыре матча', 'пять матчей'][kolvo_rush - 2];
+    let first_times = sl(1, kolvo_rush); // число выигранных жребиев
+
+    first_times = getListedPreference(key, [{
+        preference: 'equal',
+        preferenceValue: first_times,
+    }, {
+        preference: 'more_less',
+        preferenceValue: sl(1, kolvo_rush-1),
+    }], first_times);
+
+    let rovno_ili_bol = 
+    getListedPreference(key, [{
+        preference: 'equal',
+        preferenceValue: 0,
+    }, {
+        preference: 'more_less',
+        preferenceValue: 1,
+    }], (kolvo_rush === first_times) ? 0: sl1()); // ровно %количество побед% = 0, больше = 1
 
     const kolvoOptions = [
         ['ровно один раз', 'ровно два раза', 'ровно три раза', 'ровно четыре раза', 'ровно пять раз'],
-        ['болeе одного раза', 'болeе двух раз', 'болeе трёх раз', 'болeе четырёх раз', 'болeе пяти раз']
+        ['одного раза', 'двух раз', 'трёх раз', 'четырёх раз', 'пяти раз']
     ];
 
     const probabilityMatrix = {
@@ -34,7 +62,7 @@
         5: [[0.15625, 0.3125, 0.3125, 0.15625, 0.03125], [0.8125, 0.5, 0.1875, 0.03125, 0]]
     };
 
-    const kolvo = kolvoOptions[rovno_ili_bol][first_times - 1];
+    const kolvo = ['более ','не менее '].iz().esli(rovno_ili_bol) + kolvoOptions[rovno_ili_bol][first_times - 1];
     const answers = probabilityMatrix[kolvo_rush][rovno_ili_bol][first_times - 1];
 
     NAtask.setTask({
