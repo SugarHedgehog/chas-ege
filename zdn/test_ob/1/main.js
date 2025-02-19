ffcb95553141d913f3eed956ff849e2d06a1a0ca
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-02-19 15:54:55 +0300

    test_ob

---
+++ zdn/test_ob/1/main.js
@@ -0,0 +1,6 @@
+window.nomer=[
+	26592,
+    26595,
+    26597,
+].iz()
+window.comment='Тестовый пак';
---
+++ zdn/test_ob/test_ob.js
@@ -0,0 +1,7 @@
+if (!window.nabor)
+	window.nabor = {};
+window.nabor.importFrom({
+	nZad: 1,
+ 	adres: '../zdn/test_ob/',
+	name: 'test_ob',
+});
