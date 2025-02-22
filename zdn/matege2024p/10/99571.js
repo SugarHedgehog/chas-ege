caf84956507159ac74deea40e38bea430ad10d4b
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-02-22 13:00:31 +0300

    add preference 99571

--- zdn/matege2024p/10/99571.js
+++ zdn/matege2024p/10/99571.js
@@ -3,19 +3,11 @@
     retryWhileError(function () {
         /* В сосуд, содержащий 5 литров 12−процентного водного раствора некоторого вещества, добавили 7 литров воды. Сколько процентов составляет концентрация получившегося раствора? */
 		let key = '99571';
-        let rand = getListedPreference(key, [{
-			preference: 'final_concentration',
-			preferenceValue: 0,
-		}, {
-			preference: 'water_volume',
-			preferenceValue: 1,
-		}, {
-			preference: 'first_concentration',
-			preferenceValue: 2,
-		}, {
-			preference: 'first_volume',
-			preferenceValue: 3,
-		}], sl(0, 3));
+        let preference = ['final_concentration', 'water_volume', 'first_concentration', 'first_volume'];
+        let rand = getListedPreference(key, preference.map((pref, index) => ({
+			preference: pref,
+			preferenceValue: index
+		})), sl(preference.length - 1));
 		
         let firstVolume = sl(5, 50, 0.01);
         let waterVolume = slKrome(firstVolume, 5, 50, 0.01);
@@ -59,6 +51,7 @@
             ][rand]],
             postquestion: '?',
             authors: ['Александра Суматохина'],
+            preference:preference,
         });
         NAtask.modifiers.allDecimalsToStandard( /*true*/);
     }, 2000);
