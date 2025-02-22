da945c046cefed69b9edffacc9a0936f19e029b4
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-02-22 16:47:32 +0300

    misc_text_tasks_for_moving_in_straight_line

---
+++ zdn/misc_text_tasks_for_moving_in_straight_line/misc_text_tasks_for_moving_in_straight_line.js
@@ -0,0 +1,7 @@
+if (!window.nabor)
+	window.nabor = {};
+window.nabor.importFrom({
+	nZad: 14,
+ 	adres: '../zdn/misc_text_tasks_for_moving_in_straight_line/',
+	name: 'misc_text_tasks_for_moving_in_straight_line',
+});
