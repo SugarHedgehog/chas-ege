1fe9ee5a8bc39b29b56d843b8f394b5682345416
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-02-22 14:16:37 +0300

    add preference 15

--- zdn/mat2014/B14/15.js
+++ zdn/mat2014/B14/15.js
@@ -1,55 +1,54 @@
 (function() {
-	NAinfo.requireApiVersion(0, 2);
+	'use strict';
+	retryWhileError(function() {
 
-	var stations = NLsets.alphabets.russian.getRandomItems(2);
-	stations[0] += '.';
-	stations[1] += '.';
+		let key = "15";
+		let preference = ['distance', 'ratio_of_speeds'];
+		let rand = getListedPreference(key, preference.map((pref, index) => ({
+			preference: pref,
+			preferenceValue: index
+		})), sl(preference.length - 1));
 
-	var name = sklonlxkand(window.imenaj.ie.iz());
+		var stations = om.rusbukv.iz(2).map(letter => letter + '.');
 
-	do{
-		var distanceBetweenStations = sl(1.5,10,0.5);
-		var distanceToLeftStation = sl(0.100,2.000,0.001);
-		var answer = distanceBetweenStations * distanceToLeftStation / (distanceBetweenStations - 2 * distanceToLeftStation)
-	}while(!(1000*answer).isZ() || answer <= 0 || answer >= 20);
+		var name = sklonlxkand(window.imenaj.ie.iz());
 
-	var questionsAndAnswers = [
-		[
-			'Каково сейчас расстояние (в км) между ' + name.te + ' и автобусом? ',
-			answer + distanceToLeftStation,
-		],
-	];
+		do {
+			var distanceBetweenStations = sl(1.5, 10, 0.5);
+			var distanceToLeftStation = sl(0.100, 2.000, 0.001);
+			var answer = distanceBetweenStations * distanceToLeftStation / (distanceBetweenStations - 2 *
+				distanceToLeftStation)
+		} while (!(1000 * answer).isZ() || answer <= 0 || answer >= 20);
+		
+		genAssertZ1000(answer / distanceToLeftStation);
 
-	if ( (1000*answer / distanceToLeftStation).isZ() ){
-		questionsAndAnswers.push([
-			[
-				'Во сколько раз скорость автобуса больше скорости велосипеда?',
-				'Во сколько раз скорость велосипеда меньше скорости автобуса?',
-			].iz(),
-			answer / distanceToLeftStation,
-		])
-	}
-
-
-	questionsAndAnswers = questionsAndAnswers.iz();
-
-
-	NAtask.setTask({
-		text: 'Между остановками ' + stations[0] + ' и ' + stations[1] + ' ' +
-			chislitlx(distanceBetweenStations, 'километр') + ' прямой трассы. '+
-			name.ie + ' выехала на велосипеде из леса между станциями в ' +
-			chislitM(distanceToLeftStation*1000, 'метре', 'метрах', 'метрах')+
-			' от ' + stations[0] + ' и увидела, что к ' + stations[0] + ' в направлении к ' + stations[1] +
-			' с постоянной скоростью подъезжает автобус, на который ' + name.de + ' нужно успеть. '+
-			'Она заметила, что если она сейчас поедет в сторону ' + stations[0] + ', она окажется там одновременно с автобусом. ' +
-			'Но и если она поедет в сторону ' + stations[1] + ', она также окажется там одновременно с автобусом, '+
-			'который успеет преодолеть весь участок от ' + stations[0] + ' до ' + stations[1] +
-			', не останавливаясь на остановке ' + stations[0] + ' ' +
-			 questionsAndAnswers[0] +
-			' (Считайте, что автобус и велосипед движутся с постоянными скоростями и останавливаются мгновенно.)',
-		answers: questionsAndAnswers[1].ts(),
-		// TODO: во сколько раз cкорость призрака меньше скорости поезда?
-	});
+		NAtask.setTask({
+			text: 'Между остановками ' + stations[0] + ' и ' + stations[1] + ' ' +
+				chislitlx(distanceBetweenStations, 'километр') + ' прямой трассы. ' +
+				name.ie + ' выехала на велосипеде из леса между станциями в ' +
+				chislitlx(distanceToLeftStation * 1000, 'метр') +
+				' от ' + stations[0] + ' и увидела, что к ' + stations[0] + ' в направлении к ' + stations[1] +
+				' с постоянной скоростью подъезжает автобус, на который ' + name.de + ' нужно успеть. ' +
+				'Она заметила, что если она сейчас поедет в сторону ' + stations[0] +
+				', она окажется там одновременно с автобусом. ' +
+				'Но и если она поедет в сторону ' + stations[1] + ', она также окажется там одновременно с автобусом, ' +
+				'который успеет преодолеть весь участок от ' + stations[0] + ' до ' + stations[1] +
+				', не останавливаясь на остановке ' + stations[0] + ' ',
+			questions: [[{
+				text: 'Каково сейчас расстояние (в км) между ' + name.te + ' и автобусом?',
+				answers: answer + distanceToLeftStation,
+			}, {
+				text: [
+					'Во сколько раз скорость автобуса больше скорости велосипеда?',
+					'Во сколько раз скорость велосипеда меньше скорости автобуса?',
+				].iz(),
+				answers: answer / distanceToLeftStation,
+			}][rand]],
+			postquestion: ' (Считайте, что автобус и велосипед движутся с постоянными скоростями и останавливаются мгновенно.)',
+				// TODO: во сколько раз cкорость призрака меньше скорости поезда?
+			preference: preference,
+		});
+	}, 2000);
 })();
 
 //Николай Авдеев
