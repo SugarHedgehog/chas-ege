075198bdec1b398342abc933bf83b412b85fe31a
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-02-22 11:57:02 +0300

    Squashed commit of the following:
    commit 07625b1599cf5207ab53183c9fda0250c4fcff53
    Author: Aleksandra Sumatokhina <sneginca.cat@gmail.com>
    Date:   Thu Feb 20 20:24:19 2025 +0300
    
        update generateHtmlForTask with preference
    
    commit 162f6efdc9627c7df3b9c84a3451ea2ca4561cb3
    Author: Aleksandra Sumatokhina <sneginca.cat@gmail.com>
    Date:   Thu Feb 20 20:22:11 2025 +0300
    
        currentTask -> currentTaskPath
    
    commit 51de8efeb43242f49f073b539f79b5984132ec6c
    Author: Aleksandra Sumatokhina <sneginca.cat@gmail.com>
    Date:   Thu Feb 20 20:06:15 2025 +0300
    
        rez -> htmlContent
    
    commit 87af854e186150877c8d9b81cb86f0bf2b9b1672
    Author: Aleksandra Sumatokhina <sneginca.cat@gmail.com>
    Date:   Thu Feb 20 20:05:54 2025 +0300
    
        masdey -> actionsArray
    
    commit b4ce780104ec867757e5d99b1e2688c23a91d173
    Author: Aleksandra Sumatokhina <sneginca.cat@gmail.com>
    Date:   Thu Feb 20 20:05:32 2025 +0300
    
        zdn -> taskNumber
    
    commit 602f01bf65922dccb747f868ac46d28c56bb6b99
    Author: Aleksandra Sumatokhina <sneginca.cat@gmail.com>
    Date:   Wed Feb 19 15:41:47 2025 +0300
    
        kat -> category
    
    commit 631193644b1ec7b6fdeec12d6f4adf6b7d50d4ff
    Author: Aleksandra Sumatokhina <sneginca.cat@gmail.com>
    Date:   Thu Feb 20 19:55:22 2025 +0300
    
        add preference to functions

--- sh/katalog.js
+++ sh/katalog.js
@@ -1,5 +1,10 @@
-'use strict';
-
+/**
+ * Генерирует HTML для задания.
+ * @param {string} category - Категория задания.
+ * @param {string} taskNumber - Номер задания.
+ * @param {Array} actionsArray - Массив действий.
+ * @returns {string} - HTML-код задания.
+ */
 /**
  * Генерирует HTML для задания.
  * @param {string} category - Категория задания.
@@ -11,166 +16,80 @@ function generateHtmlForTask(category, taskNumber, actionsArray) {
     let htmlContent = '';
     vopr.podg();
     const currentTaskPath = `${nabor.adres}${category}/${taskNumber}.js`;
-    htmlContent += `<div class="task-wrapper" data-category="${category}" data-tasknumber="${taskNumber}">`;
-    htmlContent += currentTaskPath.vTag('h2');
-    console.log(currentTaskPath);
 
     try {
+        // Execute the task generator
         nabor.upak[category][taskNumber]();
-        vopr.template = currentTaskPath.replace(/^(\.\.\/)+/, '');
-        vopr.taskNumber = category;
-        htmlContent += `<br/>${vopr.txt.vTag('div')}<br/>`;
-        htmlContent += `
-            <div>
-                <button class="copybutton" style="float:right;" title="Экспорт в РешуЕГЭ" data-task="${encodeURIComponent(JSON.stringify(vopr))}">&#x2398;</button>
-                <button class="renewbutton" style="float:right; margin-right:1.46em;" title="Заменить задание на похожее">&#x27F3;</button>
-                <button class="addbutton" style="float:right; margin-right:1.46em;" title="Добавить похожее задание">+</button>
-                Ответ: ${vopr.ver.join('или')}
-            </div>
-            <br/>
-        `;
-        if (vopr.dey) {
-            actionsArray.push(vopr.dey);
+
+        // Handle variations if preferences exist
+        let variants = [null]; // Default case - single variant
+        if (vopr.preference && Array.isArray(vopr.preference) && vopr.preference.length > 0) {
+            variants = generateVariations(vopr.preference);
         }
 
-        if (vopr.rsh) {
-            htmlContent += `
-                <button class="spoiler-show">Показать решение</button>
-                <button class="spoiler-hide">Скрыть решение</button>
-                <div class="spoiler-body">Решение: <br/>${vopr.rsh}</div>
-            `;
-        }
+        // Generate HTML for each variant
+        for (let i = 0; i < variants.length; i++) {
+            if (variants[i] !== null) {
+                // Set preferences for this variant
+                window.nabor.preferences = window.nabor.preferences || {};
+                window.nabor.preferences[taskNumber] = variants[i];
+                // Regenerate task with new preferences
+                nabor.upak[category][taskNumber]();
+            }
 
-        if (vopr.authors && vopr.authors.length) {
+            htmlContent += `<div class="task-wrapper" data-category="${category}" data-tasknumber="${taskNumber}">`;
+            htmlContent += currentTaskPath.vTag('h2');
+
+            if (variants.length > 1) {
+                const currentVariation = Array.isArray(variants[i])
+                    ? variants[i].join(', ')
+                    : variants[i][0];
+                htmlContent += `<div class="variant-info">Вариация: ${currentVariation}</div>`;
+            }
+
+            vopr.template = currentTaskPath.replace(/^(\.\.\/)+/, '');
+            vopr.taskNumber = category;
+            htmlContent += `<br/>${vopr.txt.vTag('div')}<br/>`;
             htmlContent += `
-                <br/>
-                <div class="katalog-authors">
-                    Автор${'ы'.esli(vopr.authors.length > 1)}: &nbsp;${vopr.authors.join(', ')}
+                <div>
+                    <button class="copybutton" style="float:right;" title="Экспорт в РешуЕГЭ" data-task="${encodeURIComponent(JSON.stringify(vopr))}">&#x2398;</button>
+                    <button class="renewbutton" style="float:right; margin-right:1.46em;" title="Заменить задание на похожее">&#x27F3;</button>
+                    <button class="addbutton" style="float:right; margin-right:1.46em;" title="Добавить похожее задание">+</button>
+                    Ответ: ${vopr.ver.join('или')}
                 </div>
                 <br/>
             `;
+
+            if (vopr.dey) {
+                actionsArray.push(vopr.dey);
+            }
+
+            if (vopr.rsh) {
+                htmlContent += `
+                    <button class="spoiler-show">Показать решение</button>
+                    <button class="spoiler-hide">Скрыть решение</button>
+                    <div class="spoiler-body">Решение: <br/>${vopr.rsh}</div>
+                `;
+            }
+
+            if (vopr.authors && vopr.authors.length) {
+                htmlContent += `
+                    <br/>
+                    <div class="katalog-authors">
+                        Автор${'ы'.esli(vopr.authors.length > 1)}: &nbsp;${vopr.authors.join(', ')}
+                    </div>
+                    <br/>
+                `;
+            }
+
+            htmlContent += '</div>';
         }
     } catch (e) {
         console.error(e);
+        htmlContent += `<div class="task-wrapper error" data-category="${category}" data-tasknumber="${taskNumber}">
+            Error generating task: ${e.message}
+        </div>`;
     }
 
-    htmlContent += '</div>';
     return htmlContent;
-}
-
-/**
- * Генерирует каталог заданий.
- */
-function generateKatalog() {
-    let htmlContent = '';
-    let tableOfContents = '';
-    const actionsArray = [];
-    const lineBreak = '<br/>';
-
-    for (const category in nabor.upak) {
-        window.comment = '';
-        window.availableTaskNumbers = null;
-
-        try {
-            nabor.upak[category][nabor.scheduler]();
-        } catch (e) {
-            console.error(e);
-        }
-
-        htmlContent += `
-            <button class="spoiler-show">Показать категорию ${category}</button>
-            <button class="spoiler-hide">Скрыть категорию ${category}</button>
-            <div class="spoiler-body">
-                <h1 id="${category}">Категория ${category}</h1>
-                ${window.comment}
-        `;
-        tableOfContents += `<a href="#${category}">${category}. ${window.comment}</a>${lineBreak}`;
-
-        const tasksToList = window.availableTaskNumbers || Object.keys(nabor.upak[category]);
-
-        for (const taskNumber of tasksToList) {
-            if (taskNumber !== 'main' && taskNumber !== 'fipi') {
-                htmlContent += generateHtmlForTask(category, taskNumber, actionsArray);
-            }
-        }
-
-        htmlContent += '</div>';
-    }
-
-    $('#divrez').html(tableOfContents + lineBreak + htmlContent);
-
-    actionsArray.forEach(action => {
-        try {
-            if (typeof action === 'function') {
-                action();
-            }
-        } catch (e) {
-            console.error(e);
-        }
-    });
-
-    MathJax.Hub.Typeset();
-    afterTasksGenerated();
-    $('.spoiler-show').click();
-}
-
-/**
- * Выполняет действия после генерации заданий.
- */
-function afterTasksGenerated() {
-    spoiler();
-    $('button.copybutton[data-already-inited!=true]').click(copyTask).attr('data-already-inited', true);
-    $('button.renewbutton[data-already-inited!=true]').click(renewTask).attr('data-already-inited', true);
-    $('button.addbutton[data-already-inited!=true]').click(addTask).attr('data-already-inited', true);
-}
-
-/**
- * Копирует задание в буфер обмена.
- */
-function copyTask() {
-    console.log(this);
-    let taskData = decodeURIComponent(this.getAttribute('data-task'));
-    console.log(taskData);
-    try {
-        taskData = JSON.parse(taskData);
-    } catch (e) {
-        console.error('Failed to parse task data:', e);
-        return;
-    }
-    console.log(taskData);
-
-    replaceCanvasWithImgInTaskAndHTML($(this).closest('div.task-wrapper')[0], taskData, () => {
-        const fillerCode = createFiller(taskData);
-        copyToClipboard(fillerCode);
-    });
-}
-
-/**
- * Обновляет задание на новое.
- */
-function renewTask() {
-    console.log(this);
-    const taskWrapper = $(this).closest('div.task-wrapper')[0];
-    const actions = [];
-    const taskHtml = $(generateHtmlForTask(taskWrapper.getAttribute('data-category'), taskWrapper.getAttribute('data-tasknumber'), actions));
-    $(taskWrapper).replaceWith(taskHtml);
-    if (actions[0] && typeof actions[0] === 'function') {
-        actions[0]();
-    }
-    MathJax.Hub.Typeset(taskHtml[0]);
-    afterTasksGenerated();
-}
-
-/**
- * Добавляет новое задание после текущего.
- */
-function addTask() {
-    console.log(this);
-    const taskWrapper = $(this).closest('div.task-wrapper')[0];
-    const actions = [];
-    const taskHtml = $(generateHtmlForTask(taskWrapper.getAttribute('data-category'), taskWrapper.getAttribute('data-tasknumber'), actions));
-    taskHtml.insertAfter(taskWrapper);
-    actions[0]();
-    MathJax.Hub.Typeset(taskHtml[0]);
-    afterTasksGenerated();
 }
