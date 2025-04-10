385b24fedc35743b6f8898ad7034c4269168011f
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-04-10 13:14:09 +0300

    10173 add

--- zdn/misc_planimetry_base_pythagorean_theorem_2025/misc_planimetry_base_pythagorean_theorem_2025.js
+++ zdn/misc_planimetry_base_pythagorean_theorem_2025/misc_planimetry_base_pythagorean_theorem_2025.js
@@ -1,7 +1,7 @@
 if (!window.nabor)
 	window.nabor = {};
 window.nabor.importFrom({
-	nZad: 26,
+	nZad: 27,
  	adres: '../zdn/misc_planimetry_base_pythagorean_theorem_2025/',
 	name: 'misc_planimetry_base_pythagorean_theorem_2025',
 });
