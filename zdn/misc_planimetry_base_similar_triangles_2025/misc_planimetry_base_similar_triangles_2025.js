ebcca9b4173be7bd726472582d4544251e276e2f
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-05-09 13:49:42 +0300

    misc_planimetry_base_similar_triangles_2025

---
+++ zdn/misc_planimetry_base_similar_triangles_2025/misc_planimetry_base_similar_triangles_2025.js
@@ -0,0 +1,7 @@
+if (!window.nabor)
+	window.nabor = {};
+window.nabor.importFrom({
+	nZad: 6,
+ 	adres: '../zdn/misc_planimetry_base_similar_triangles_2025/',
+	name: 'misc_planimetry_base_similar_triangles_2025',
+});
