45e31e5440f684d81650c033a7dd808692e39b8f
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-06-19 18:41:01 +0300

    Add task reference numbers and parameter notes for quadrilateral problem

--- zdn/misc_planimetry_base_quadrilateral_2025/misc_planimetry_base_quadrilateral_2025.js
+++ zdn/misc_planimetry_base_quadrilateral_2025/misc_planimetry_base_quadrilateral_2025.js
@@ -5,3 +5,14 @@ window.nabor.importFrom({
  	adres: '../zdn/misc_planimetry_base_quadrilateral_2025/',
 	name: 'misc_planimetry_base_quadrilateral_2025',
 });
+/* 
+6563
+11512
+2547
+2650
+506683
+"512423 меньшая_диагональ small_diagonal"
+"512423 периметр perimeter"
+510126
+514394
+ */
