d8ce7b11adb8c24f60836af3a42a086359c50edb
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-06-19 18:08:26 +0300

    Squashed commit of the following:
    commit 147096dc57212c50808e1756b29fc63d5ccab0e3
    Author: Aleksandra Sumatokhina <sneginca.cat@gmail.com>
    Date:   Sun Jun 8 08:54:28 2025 +0300
    
        misc_base_other
    
    commit 9a2a976b171244e20a691e9e819218f8ae55a58d
    Author: Zer00Player <luly3751@gmail.com>
    Date:   Sun May 18 12:58:42 2025 +0300
    
        [lib]coordAxis-a (#2463)
    
        * [lib]coordAxis-a
    
        * back to original strokeStyle;
        back to original fillStyle;
        add options;
    
        * add descriptions
    
    commit a24e8f6fa5376cd86d901f1001dfe2c84fdd98cd
    Author: Aleksandra Sumatokhina <sneginca.cat@gmail.com>
    Date:   Thu Apr 24 12:16:08 2025 +0300
    
        fix pechmat
    
    commit 25db8c4adab67efaeb79b6beb22ed37daee4cc50
    Author: Aleksandra Sumatokhina <sneginca.cat@gmail.com>
    Date:   Sun May 4 09:57:13 2025 +0300
    
        generate_preference_list
    
    commit 95ebcb32f16be06c5f79ab3106b134d7675bd31d
    Author: Aleksandra Sumatokhina <sneginca.cat@gmail.com>
    Date:   Wed Apr 23 16:29:33 2025 +0300
    
        fix #2432
    
    commit b9f24f45cd1f13098dda20a15b22e2ea4cbc15c1
    Author: Aleksandra Sumatokhina <sneginca.cat@gmail.com>
    Date:   Sat Apr 19 10:52:47 2025 +0300
    
        other rebase
    
    commit 7e7cb7a79f3aad684eab5973cfbb0e73c252007c
    Author: Aleksandra Sumatokhina <sneginca.cat@gmail.com>
    Date:   Sat Apr 19 10:52:40 2025 +0300
    
        zdn rebase
    
    commit c121ebe3030912be88d038183c9862b1f269177c
    Author: Aleksandra Sumatokhina <sneginca.cat@gmail.com>
    Date:   Sat Apr 19 10:52:16 2025 +0300
    
        src rebase
    
    commit fd01612c179e0c475513135769097ee5a1df3297
    Author: Aleksandra Sumatokhina <sneginca.cat@gmail.com>
    Date:   Sat Apr 19 10:52:02 2025 +0300
    
        sh rebase
    
    commit a518a2cd6dbc9ba12cd03a9276b771e27ceddea1
    Author: Aleksandra Sumatokhina <sneginca.cat@gmail.com>
    Date:   Sat Apr 19 10:51:54 2025 +0300
    
        lib rebase

---
+++ zdn/misc_base_other_2025/misc_base_other_2025.js
@@ -0,0 +1,19 @@
+if (!window.nabor)
+	window.nabor = {};
+window.nabor.importFrom({
+	nZad: 5,
+ 	adres: '../zdn/misc_base_other_2025/',
+	name: 'misc_base_other_2025',
+});
+
+/* 
+6563
+11512
+2547
+2650
+506683
+"512423 меньшая_диагональ small_diagonal"
+"512423 периметр perimeter"
+510126
+514394 
+*/
