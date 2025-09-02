316a42a6397dea4a213da84310e303b739216766
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-09-02 17:22:35 +0300

    misc_exp_trans_2025

---
+++ zdn/misc_exp_trans_2025/misc_exp_trans_2025.js
@@ -0,0 +1,7 @@
+if (!window.nabor)
+	window.nabor = {};
+window.nabor.importFrom({
+	nZad: 32,
+ 	adres: '../zdn/misc_exp_trans_2025/',
+	name: 'misc_exp_trans_2025',
+});
--- zdn/misc_expression_transformations_2025/misc_expression_transformations_2025.js
+++
@@ -1,7 +0,0 @@
-if (!window.nabor)
-	window.nabor = {};
-window.nabor.importFrom({
-	nZad: 32,
- 	adres: '../zdn/misc_expression_transformations_2025/',
-	name: 'misc_expression_transformations_2025',
-});
