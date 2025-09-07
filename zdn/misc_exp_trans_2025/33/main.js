f07c94145838565932b83e0b383aaefc83ed2b9e
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-09-07 13:47:39 +0300

    26755

--- zdn/misc_exp_trans_2025/33/main.js
+++ zdn/misc_exp_trans_2025/33/main.js
@@ -2,5 +2,5 @@ window.nomer=[
 	11,
 ].iz()
 window.comment='11 integer double angle without tg ctg';
-window.nabor.preferences['11'] = ['integer','double_angle', 'without_tg_ctg'];
+window.nabor.preferences['26755'] = ['integer','double_angle', 'without_tg_ctg'];
 
