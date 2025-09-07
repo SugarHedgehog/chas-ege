a08a4c2e2772f2bd89b8cd0cdb30327c4ab57a03
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-09-07 13:06:43 +0300

    without_tg_ctg

--- zdn/mat2014/B11/11.js
+++ zdn/mat2014/B11/11.js
@@ -5,15 +5,17 @@
 		let key = '26755';
 		let preference1 = ['integer', 'not_integer'];
 		let preference2 = ['double_angle', 'reduction_formula'];
+		let preference3 = ['with_tg_ctg', 'without_tg_ctg'];
 		let rand1 = getSelectedPreferenceFromList(key, preference1);
 		let rand2 = getSelectedPreferenceFromList(key, preference2);
+		let rand3 = getSelectedPreferenceFromList(key, preference3);
 
 		var a = slKrome([90, 180, 270], 1, 359);
 		var at = '{' + a.ts() + '^\\circ}';
-		var b = [sl(2, 400), sl(2, 400)+sl(1, 9)/10][rand1].pm();
+		var b = [sl(2, 400), sl(2, 400) + sl(1, 9) / 10][rand1].pm();
 		var c = (2).pow(sl(-2, 4)) * (5).pow(sl(-2, 4)).pm();
-		
-		if(rand1==0){
+
+		if (rand1 == 0) {
 			genAssertAlmostInteger(c);
 		}
 
@@ -21,7 +23,9 @@
 			['\\sin', '\\cos'],
 			['\\tg', '\\cos^2'],
 			['\\sin^2', '\\ctg']
-		].iz().shuffle();
+		];
+		
+		f = (rand3? f[0] : f = f[sl1()+1]).shuffle();
 
 		var vyr1 = [f[0] + at, f[1] + at].shuffle().join('\\cdot');
 		var vyr2 = [
@@ -29,19 +33,21 @@
 				'\\sin' + (360 + 2 * a).ts() + '^\\circ',
 				'\\cos' + (90 - 2 * a).negativeBrackets() + '^\\circ',
 				'\\cos' + (450 - 2 * a).negativeBrackets() + '^\\circ',
-			][[0, sl(1,3)][rand2]]
+			][
+				[0, sl(1, 3)][rand2]
+			]
 			.replace(')^\\circ', '^\\circ)'); // A sever co-style! TODO!
 
 		var y, answer;
 		if (sl1()) {
-			y = '\\frac{' + b.ts() + '~' + vyr1 + '}{' + c.ts().esli(c!=1) + '~' + vyr2 + '}';
+			y = '\\frac{' + b.ts() + '~' + vyr1 + '}{' + c.ts().esli(c != 1) + '~' + vyr2 + '}';
 			answer = b / (2 * c);
 		} else {
 			y = '\\frac{' + b.ts() + '~' + vyr2 + '}{' +
-				c.ts().esli(c!=1) + '~' + vyr1 + '}';
+				c.ts().esli(c != 1) + '~' + vyr1 + '}';
 			answer = b * 2 / c;
 		}
-		
+
 		genAssertZ1000(answer);
 
 		chas2.task.setTask({
--- zdn/misc_exp_trans_2025/33/main.js
+++ zdn/misc_exp_trans_2025/33/main.js
@@ -1,6 +1,6 @@
 window.nomer=[
 	11,
 ].iz()
-window.comment='11 integer double angle';
-window.nabor.preferences['11'] = ['integer','double_angle'];
+window.comment='11 integer double angle without tg ctg';
+window.nabor.preferences['11'] = ['integer','double_angle', 'without_tg_ctg'];
 
