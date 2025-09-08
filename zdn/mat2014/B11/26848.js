802ee98ebb199da8466d69eb6ef3f1f98c555e17
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-09-08 12:30:40 +0300

    [zdn] [up] - 26848.js

--- zdn/mat2014/B11/26848.js
+++ zdn/mat2014/B11/26848.js
@@ -1,25 +1,43 @@
 (function() {
-	NAinfo.requireApiVersion(0, 0);
-	var b = 1; // Основание для логарифма 
-	var a = 1; // Подлогарифмическое первого 
-	var z = [0.5, 0.25, 0.125].iz();
-	var c = sluchch(1, 10, z); // Подлогарифмическое второго
-	var y = 1;
-	do {
-		a = sluchch(2*c, 100, c);
-		y = [1,2,4,5].iz();
-		b = Math.pow(a / c, y);
-	}
-	while (b > 100);
-	NAtask.setTask({
-		text: 'Найдите значение выражения $$\\log_{' + b + '}{' + a + '} - \\log_{' + b + '}{' + c + '} $$',
-		answers: 1 / y,
-	}, {
-		tags: {
-			log: 1,
-		},
-	});
+	retryWhileError(function() {
+		NAinfo.requireApiVersion(0, 2);
+
+		let key = '26848';
+		let preference = ['integer', 'not_integer', 'frac'];
+		let rand = getSelectedPreferenceFromList(key, preference);
+
+		let b = [sl(2, 10).pow(sl(1, 3)), [2, 4, 5, 10].iz().pow(-1), sl(2, 16)][rand];
+		let bText = b;
+
+		if (rand == 2) {
+			bText = (1).texfrac(b);
+			b = 1 / b;
+		}
+
+		if (rand == 1)
+			genAssertZ1000(b / 10);
+
+		let answ = sl(0.5, 5, 0.5).pm();
+		let subansw = b.pow(answ);
+		let a = sl(1, 100, 0.5);
+		let c = a / subansw;
+
+		genAssertZ1000(c);
+		genAssert((c + '').length < 5, '');
+
+		NAtask.setTask({
+			text: 'Найдите значение выражения: $$\\log_{' + bText + '}{' + a + '} - \\log_{' + bText + '}{' + c + '} $$',
+			answers: answ,
+		}, {
+			tags: {
+				log: 1,
+			},
+			preference: preference,
+		});
+		NAtask.modifiers.allDecimalsToStandard(true);
+	}, 1000);
 })();
+
 //https://math-ege.sdamgia.ru/problem?id=26848
 //Автор: Арахов Никита
 //Reviewed and commited by Aisse-258
