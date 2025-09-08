708268c962dff7b22c273460f4ee53b5d331fe48
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-09-08 12:30:06 +0300

    [zdn] [up] - 5.js

--- zdn/mat2014/B11/5.js
+++ zdn/mat2014/B11/5.js
@@ -1,25 +1,31 @@
 (function() {
-	'use strict';
+	retryWhileError(function() {
+		'use strict';
+	let key = '26738';
+	let preference = ['same_base', 'diff_base'];
+	let rand = getSelectedPreferenceFromList(key, preference);
 
 	var a = sluchch(2, 19),
 		b = sluchch(1, 3),
 		c = sluchch(0.01, 0.99, 0.01),
-		d = sluchch(1, 2),
+		d = [b, slKrome(b, 1, 2)][rand],
 		z = sluchch(1, 4),
 		f = (z - b * c) / d,
 		m = [
-				[
-					a.pow(b).toFixedLess(5).toStandart(),
-					c.toFixedLess(5).toStandart()
-				],
-				[
-					a.pow(d).toFixedLess(5).toStandart(),
-					f.toFixedLess(5).toStandart()
-				],
+			[
+				a.pow(b).toFixedLess(5).toStandart(),
+				c.ts()
+			],
+			[
+				a.pow(d).toFixedLess(5).toStandart(),
+				f.ts()
+			],
 		].shuffle();
+		
+	genAssertZ1000(f);
 
 	chas2.task.setTask({
-		text: ('Найдите значение выражения $$'+m[0][0]+'^{'+m[0][1]+'}\\cdot'+m[1][0]+'^{'+m[1][1]+'}$$').plusminus(),
+		text: ('Найдите значение выражения $$' + m[0][0] + '^{' + m[0][1] + '}\\cdot' + m[1][0] + '^{' + m[1][1] + '}$$').plusminus(),
 		answers: a.pow(z),
 		tags: {
 			'log': 0,
@@ -27,6 +33,8 @@
 			'drs': 1,
 			'tri': 0,
 		},
-	});
-
+		});
+	},100);
 })();
+
+//https://math-ege.sdamgia.ru/problem?id=26738
