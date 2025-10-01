d1d6b1fc08d03b36310cc3d978fcd790405aad24
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-10-01 16:08:38 +0300

    misc_tasks_with_applied_content_2026_fipi

---
+++ zdn/misc_tasks_with_applied_content_2026_fipi/8/main.js
@@ -0,0 +1,5 @@
+window.nomer=[
+	27975,
+].iz()
+window.comment='27975';
+chas2.task.setTask.forbidDecimalFractions = true;
---
+++ zdn/misc_tasks_with_applied_content_2026_fipi/misc_tasks_with_applied_content_2026_fipi.js
@@ -0,0 +1,7 @@
+if (!window.nabor)
+	window.nabor = {};
+window.nabor.importFrom({
+	nZad: 21,
+ 	adres: '../zdn/misc_tasks_with_applied_content_2026_fipi/',
+	name: 'misc_tasks_with_applied_content_2026_fipi',
+});
