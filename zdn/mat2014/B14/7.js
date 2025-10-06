4f50d6be85cc333934ef962a8620b765824b74e7
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-10-06 12:04:37 +0300

    [zdn] [up] - 7

--- zdn/mat2014/B14/7.js
+++ zdn/mat2014/B14/7.js
@@ -1,20 +1,20 @@
 (function() {
 	let key = "7";
-	let preference = ['river_speed','boat_speed','time_difference','total_distance'];
-	let variant = getListedPreference(key, preference.map((pref, index) => ({
-		preference: pref,
-		preferenceValue: index
-	})), sl(preference.length - 1));
-
+	let preference1 = ['river_speed', 'boat_speed', 'time_difference', 'total_distance', 'half_distance'];
+	let preference2 = ['have_total_distance', 'have_half_distance'];
+	let variant1 = getSelectedPreferenceFromList(key, preference1);
 	
-	var b = 0.5, x, a, s;
+	let variant2 = variant1 > 2? variant1%3: getSelectedPreferenceFromList(key, preference2);
+
+	var b = 0.5,
+		x, a, s;
 	for (; !(b.isZ() && b > 0);) {
 		x = sluchch(5, 20);
 		a = sluchch(2, x - 1);
 		s = sluchch(10, 200);
 		b = s / (x - a) - s / (x + a);
 	}
-	 
+
 	/*
 	Моторная лодка прошла против течения реки 112 км и вернулась в пункт отправления,
 	затратив на обратный путь на 6 часов меньше.
@@ -26,31 +26,37 @@
 		vel: 'скорость течения',
 		zna: a,
 		rod: 1,
-		nah: (variant == 0),
+		nah: (variant1 == 0),
 		nmn: 'км/ч',
 	}, {
 		vel: 'скорость лодки в неподвижной воде',
 		zna: x,
 		rod: 1,
-		nah: (variant == 1),
+		nah: (variant1 == 1),
 		nmn: 'км/ч'
 	}, {
 		utv: 'лодка затратила на обратный путь на ' + chislitM(b, 'час', 'часа', 'часов') + ' меньше',
 		vpr: 'насколько меньше времени затратила лодка на обратный путь',
 		zna: b,
-		nah: (variant == 2)
-	}, {
+		nah: (variant1 == 2)
+	}, [{
 		vel: 'суммарное пройденное лодкой расстояние',
 		zna: s * 2,
 		rod: 2,
-		nah: (variant == 3),
+		nah: (variant1 == 3),
 		nmn: 'км'
-	}, ], {
+	}, {
+		vel: 'пройденное лодкой расстояние '+['по течению', 'против течения'].iz(),
+		zna: s,
+		rod: 2,
+		nah: (variant1 == 4),
+		nmn: 'км'
+	}][variant2]], {
 		preambula: 'Моторная лодка прошла против течения реки и вернулась в пункт отправления. ',
-		preference: preference,
+		preference1: preference1,
 	});
 })();
-	 
+
 //Обзад 26585 26586
 //Николай Авдеев
 //Рефактринг проводил  Shellge
