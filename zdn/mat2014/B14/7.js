3d8925256530c2245bf6d99f4d5265a707157058
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-10-06 12:04:37 +0300

    [zdn] [up] - 26585

--- zdn/mat2014/B14/7.js
+++ zdn/mat2014/B14/7.js
@@ -1,4 +1,8 @@
 (function() {
+	let key = "26585";
+	let preference1 = ['river_speed', 'boat_speed', 'time_difference', 'total_distance', 'half_distance'];
+	let preference2 = ['have_total_distance', 'have_half_distance'];
+	let variant1 = getSelectedPreferenceFromList(key, preference1);
 	var b = 0.5, x, a, s;
 	for (; !(b.isZ() && b > 0);) {
 		x = sluchch(5, 20);
@@ -24,21 +28,28 @@
 		vel: 'скорость лодки в неподвижной воде',
 		zna: x,
 		rod: 1,
-		nah: 1,
+		nah: (variant == 1),
 		nmn: 'км/ч'
 	}, {
 		utv: 'лодка затратила на обратный путь на ' + chislitlx(b, 'час') + ' меньше',
 		vpr: 'насколько меньше времени затратила лодка на обратный путь',
 		zna: b,
-		nah: 1
+		nah: (variant == 2)
 	}, {
 		vel: 'суммарное пройденное лодкой расстояние',
 		zna: s * 2,
 		rod: 2,
-		nah: 1,
+		nah: (variant == 3),
 		nmn: 'км'
-	}, ], {
+	}, {
+		vel: 'пройденное лодкой расстояние '+['по течению', 'против течения'].iz(),
+		zna: s,
+		rod: 2,
+		nah: (variant1 == 4),
+		nmn: 'км'
+	}][variant2], {
 		preambula: 'Моторная лодка прошла против течения реки и вернулась в пункт отправления. ',
+		preference: preference,
 	});
 })();
 	 
