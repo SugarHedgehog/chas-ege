4e58ab80b80d05303cf0f065a8c2faf18d94134c
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-10-08 17:34:05 +0300

    fix key

--- zdn/misc_text_task_2026/23/main.js
+++ zdn/misc_text_task_2026/23/main.js
@@ -2,5 +2,5 @@ window.nomer=[
 	1,
 ].iz()
 window.comment='1 second largest find third mass';
-window.nabor.preferences['1'] = ['second_largest','find_third_mass'];
+window.nabor.preferences['512333'] = ['second_largest','find_third_mass'];
 chas2.task.setTask.forbidDecimalFractions = true;
--- zdn/misc_text_task_2026/24/main.js
+++ zdn/misc_text_task_2026/24/main.js
@@ -2,5 +2,5 @@ window.nomer=[
 	1,
 ].iz()
 window.comment='1 first largest find third mass';
-window.nabor.preferences['1'] = ['first_largest','find_third_mass'];
+window.nabor.preferences['512333'] = ['first_largest','find_third_mass'];
 chas2.task.setTask.forbidDecimalFractions = true;
