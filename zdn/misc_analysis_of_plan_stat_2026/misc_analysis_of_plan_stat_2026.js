36da230baff9e48fa478ab2356d13aba01eb0a72
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-10-13 10:40:49 +0300

    misc_analysis_of_plan_stat_2026

---
+++ zdn/misc_analysis_of_plan_stat_2026/misc_analysis_of_plan_stat_2026.js
@@ -0,0 +1,7 @@
+if (!window.nabor)
+	window.nabor = {};
+window.nabor.importFrom({
+	nZad: 6,
+ 	adres: '../zdn/misc_analysis_of_plan_stat_2026/',
+	name: 'misc_analysis_of_plan_stat_2026',
+});
--- zdn/misc_analysis_of_planimetric_statements_2026/misc_analysis_of_planimetric_statements_2026.js
+++
@@ -1,7 +0,0 @@
-if (!window.nabor)
-	window.nabor = {};
-window.nabor.importFrom({
-	nZad: 6,
- 	adres: '../zdn/misc_analysis_of_planimetric_statements_2026/',
-	name: 'misc_analysis_of_planimetric_statements_2026',
-});
