56b8674e277b0b42701a363d155755e05f76d936
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-10-17 15:21:00 +0300

    Merge branch 'newTask_510130'

---
+++ dev/nabor-override/nabor/nabor-override-1.js
@@ -0,0 +1,53 @@
+console.log('Overriding...');
+// Здесь можно писать комментарии. Собственно, вот так!
+// Это у нас так номер варианта пишется!
+$('#variantPrefix').val('1.');
+// Количество вариантов
+$('#cV').val('5');
+window.nabor.upak[1].main = function(){
+window.nomer = 135;
+window.nabor.preferences['135'] = ['angleACO', 'angleACO'];
+}
+window.nabor.upak[2].main = function(){
+window.nomer = 644849;
+}
+window.nabor.upak[3].main = function(){
+window.nomer = 27118;
+window.nabor.preferences['27118'] = ['cylinderIsWider', 'cylinderIsWider'];
+}
+window.nabor.upak[4].main = function(){
+window.nomer = 4;
+window.comment='4 haveDefectInQuest notHaveDefect';
+window.nabor.preferences['4'] = ['haveDefectInQuest', 'notHaveDefect'];
+}
+window.nabor.upak[5].main = function(){kf
+window.nomer = 320172;
+}
+window.nabor.upak[6].main = function(){
+window.nomer = 77384;
+}
+window.nabor.upak[7].main = function(){
+window.nomer = 26777;
+window.comment='26777 tg cos';
+window.nabor.preferences['26777'] = ['tg', 'cos'];
+}
+window.nabor.upak[8].main = function(){
+window.nomer = 2542;
+window.nabor.preferences['2542'] = ['number', 'number'];
+}
+window.nabor.upak[9].main = function(){
+window.nomer = 27996;
+window.nabor.preferences['27996'] = ['findV', 'findV'];
+}
+window.nabor.upak[10].main = function(){
+window.nomer = 99591;
+}
+window.nabor.upak[11].main = function(){
+window.nomer = 508911;
+window.nabor.preferences['508911'] = ['dontShow', 'dontShow'];
+}
+window.nabor.upak[12].main = function(){
+window.nomer = 26695;
+window.comment='26695 косинус максимум';
+window.nabor.preferences['26695'] = ['cos', 'maximum'];
+}
---
+++ dev/nabor-override/nabor/nabor-override-5.js
@@ -0,0 +1,55 @@
+console.log('Overriding...');
+// Здесь можно писать комментарии. Собственно, вот так!
+// Это у нас так номер варианта пишется!
+$('#variantPrefix').val('5.');
+// Количество вариантов
+$('#cV').val('5');
+window.nabor.upak[1].main = function(){
+window.nomer = 11;
+window.nabor.preferences['11'] = ['inscribedAngle', 'inscribedAngle'];
+}
+window.nabor.upak[2].main = function(){
+window.nomer = 660709;
+}
+window.nabor.upak[3].main = function(){
+window.nomer = 27068;
+window.comment='27068 sideSurfaceArea originalPrism';
+window.nabor.preferences['27068'] = ['sideSurfaceArea', 'originalPrism'];
+}
+window.nabor.upak[4].main = function(){
+window.nomer = 320183;
+window.comment='320183 три матча меньше';
+window.nabor.preferences['320183'] = ['three_games', 'less'];
+}
+window.nabor.upak[5].main = function(){
+window.nomer = 320211;
+}
+window.nabor.upak[6].main = function(){
+window.nomer = 5;
+window.comment='5 no frac';
+window.nabor.preferences['5'] = ['no_frac'];
+}
+window.nabor.upak[7].main = function(){
+window.nomer = 26740;
+}
+window.nabor.upak[8].main = function(){
+window.nomer = 27505;
+window.nabor.preferences['27505'] = ['kMoreZero', 'kMoreZero'];
+}
+window.nabor.upak[9].main = function(){
+window.nomer = 27965;
+}
+window.nabor.upak[10].main = function(){
+window.nomer = 8;
+window.comment='8 river speed';
+window.nabor.preferences['8'] = ['river_speed'];
+}
+window.nabor.upak[11].main = function(){
+window.nomer = 509192;
+window.comment='509192 findAbscissa withoutB2';
+window.nabor.preferences['509192'] = ['findAbscissa', 'withoutB2'];
+}
+window.nabor.upak[12].main = function(){
+window.nomer = 26719;
+window.nabor.preferences['26719'] = ['максимум', 'maximum'];
+}
