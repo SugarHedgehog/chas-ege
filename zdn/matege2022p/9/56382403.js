3c5c396522c6ec21e50e0ff1cd196c2e2bd3c6c4
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-11-07 12:49:03 +0300

    [zdn] [fix] - 56382403

--- zdn/matege2022p/9/56382403.js
+++ zdn/matege2022p/9/56382403.js
@@ -1,4 +1,4 @@
-retryWhileUndefined(function() {
+retryWhileUndefined(function () {
 	NAinfo.requireApiVersion(0, 2);
 
 	function f(x) {
@@ -30,16 +30,16 @@ retryWhileUndefined(function() {
 	let p1 = intPoints(f1, {
 		minX: limits[0][0],
 		maxX: limits[0][1],
-		minY: -6,
-		maxY: 5,
+		minY: -8,
+		maxY: 7,
 	});
 	if (p1.length < 2)
 		return;
 	let p2 = intPoints(f2, {
 		minX: limits[1][0],
 		maxX: limits[1][1],
-		minY: -6,
-		maxY: 5,
+		minY: -8,
+		maxY: 7,
 	});
 	if (p2.length < 2)
 		return;
@@ -47,19 +47,19 @@ retryWhileUndefined(function() {
 
 	let question, answ;
 	switch (sl(1, 2)) {
-	case 1:
-		let x = sl(6, 10).pm();
-		question = '$f(' + x + ')$';
-		answ = f(x);
-		break;
-	case 2:
-		let st = [
-			['произведение $k \\cdot b \\cdot c$', k * b * c],
-			['сумму $k+b+c$', k + b + c]
-		].iz();
-		answ = st[1];
-		question = st[0];
-		break;
+		case 1:
+			let x = sl(6, 10).pm();
+			question = '$f(' + x + ')$';
+			answ = f(x);
+			break;
+		case 2:
+			let st = [
+				['произведение $k \\cdot b \\cdot c$', k * b * c],
+				['сумму $k+b+c$', k + b + c]
+			].iz();
+			answ = st[1];
+			question = st[0];
+			break;
 	}
 	let sign = [
 		['>', ' \\geq '].iz(), ['>', '\\geq'].iz()
@@ -68,9 +68,9 @@ retryWhileUndefined(function() {
 		sign[0] = ['<', ' \\leq '].iz();
 	if (b < 0)
 		sign[1] = ['<', ' \\leq '].iz();
-	let paint1 = function(ct) {
-		let h = 300;
-		let w = 300;
+	let paint1 = function (ct) {
+		let h = 400;
+		let w = 400;
 		//Оси координат
 		ct.drawCoordPlane(w, h, {
 			hor: 1,
@@ -84,10 +84,10 @@ retryWhileUndefined(function() {
 		ct.scale(20, -20);
 		ct.lineWidth = 0.1;
 		graph9AdrawFunction(ct, f, {
-			minX: -6.3,
-			maxX: 7,
-			minY: -7,
-			maxY: 6,
+			minX: -8.5,
+			maxX: 8.5,
+			minY: -9.5,
+			maxY: 7.7,
 			step: 0.05,
 		});
 		//точки
@@ -102,9 +102,9 @@ retryWhileUndefined(function() {
 		analys: '$f(x)=|' + (k + 'x+' + b + '|+' + c).plusminus() + '$',
 		authors: ['Александра Суматохина'],
 	});
-	chas2.task.modifiers.addCanvasIllustration({
-		width: 300,
-		height: 300,
+	NAtask.modifiers.addCanvasIllustration({
+		width: 400,
+		height: 400,
 		paint: paint1,
 	});
 	return true;
