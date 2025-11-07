bee547596e0d3294084609b97001147074fcb891
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-11-07 12:48:42 +0300

    [zdn] [fix] - 50927501

--- zdn/matege2022p/9/50927501.js
+++ zdn/matege2022p/9/50927501.js
@@ -1,4 +1,4 @@
-retryWhileUndefined(function() {
+retryWhileUndefined(function () {
 	NAinfo.requireApiVersion(0, 2);
 
 	function pryam(k, b, x) {
@@ -18,29 +18,29 @@ retryWhileUndefined(function() {
 	}
 
 	let x1 = Math.pow(sluchch(0, 10, 0.5), 2);
-	let c = sluchch(-10,10);
+	let c = sluchch(-10, 10);
 	let a = sluchch(1, 20).pm();
 	let y1 = a * x1.sqrt() + c;
-	let k = sluchch(0.25, 20,0.25).pm();
+	let k = sluchch(0.25, 20, 0.25).pm();
 	let b = y1 - k * x1;
 	if (a * a - 4 * k * (b - c) != 0)
 		return;
 	if (x1.abs() < 7 && y1.abs() < 7 && x1.isZ() && y1.isZ())
 		return; // Точку видно
 	let pointss = intPoints(fs, {
-		minX: -5,
-		maxX: 5,
-		minY: -5.5,
-		maxY: 5.5,
+		minX: -8,
+		maxX: 8,
+		minY: -9,
+		maxY: 7,
 		step: 1,
 	});
 	if (pointss.length < 2)
 		return;
 	let pointsp = intPoints(fp, {
-		minX: -5,
-		maxX: 5,
-		minY: -5.5,
-		maxY: 5.5,
+		minX: -8,
+		maxX: 8,
+		minY: -9,
+		maxY: 7,
 		step: 1,
 	});
 	if (pointsp.length < 2)
@@ -53,11 +53,11 @@ retryWhileUndefined(function() {
 		answ = y1;
 		find = 'ординату';
 	}
-	let paint1 = function(ct) {
-		let h = 300;
-		let w = 300;
+	let paint1 = function (ct) {
+		let h = 400;
+		let w = 400;
 		//Оси координат
-		ct.drawCoordinatePlane (w, h, {
+		ct.drawCoordinatePlane(w, h, {
 			hor: 1,
 			ver: 1
 		}, {
@@ -69,17 +69,17 @@ retryWhileUndefined(function() {
 		ct.scale(20, -20);
 		ct.lineWidth = 0.1;
 		graph9AdrawFunction(ct, fs, {
-			minX: -6.5,
-			maxX: 7,
-			minY: -7,
-			maxY: 6,
+			minX: -8.5,
+			maxX: 8.5,
+			minY: -9.5,
+			maxY: 7.7,
 			step: 0.01,
 		});
 		graph9AdrawFunction(ct, fp, {
-			minX: -6.5,
-			maxX: 7,
-			minY: -7,
-			maxY: 6,
+			minX: -8.5,
+			maxX: 8.5,
+			minY: -9.5,
+			maxY: 7.7,
 			step: 0.05,
 		});
 		//точки
@@ -94,11 +94,11 @@ retryWhileUndefined(function() {
 			'$g(x)=' + (k + 'x+' + b).plusminus() + '$<br>' +
 			'$A(' + x1 + ';' + y1 + ')$<br>',
 	});
-	chas2.task.modifiers.addCanvasIllustration({
-		width: 300,
-		height: 300,
+	NAtask.modifiers.addCanvasIllustration({
+		width: 400,
+		height: 400,
 		paint: paint1,
 	});
 	return true;
 }, 100000);
-//509275
+//50927501
