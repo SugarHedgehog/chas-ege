22425139523b2b636745bbe0182781ff8404d001
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-11-11 13:02:12 +0300

    [zdn] [ref] - 509095.js

--- zdn/matege2022p/9/509095.js
+++ zdn/matege2022p/9/509095.js
@@ -1,78 +1,83 @@
-retryWhileUndefined(function () {
-	NAinfo.requireApiVersion(0, 2);
-	function axb(a, x, b) {
-		if (a > 0)
-			return Math.pow(a, x) + b;
-		return -Math.pow(a.abs(), x) + b;
-	}
+(function () {
+	retryWhileError(function () {
+		NAinfo.requireApiVersion(0, 2);
 
-	function f(x) {
-		return axb(a, x, b);
-	}
-	let a = slKrome([1], 0.25, 20, 0.25).pm();
-	let b = sluchch(1, 10).pm();
-	let chisl = sluchch(0, 5).pm();
-	if (f(chisl).abs() < 6 && f(chisl).isZ())
-		return;
-	//если находится в видимой части и целое(можно определить по графику)
-	if (!(1000 * f(chisl)).isZ())
-		return;
-	if (f(chisl).abs() > 10000)
-		return;
-	//слишком большое
-	let find, answ;
-	if (sl1()) {
-		find = `$f(${chisl.ts()})$`;
-		answ = f(chisl);
-	} else {
-		answ = chisl;
-		find = `значение $x$, при котором $f(x)=${f(chisl).ts()}$`;
-	}
-	let X = [],
-		Y = [];
-	for (let i = -8; i < 8; i++)
-		if (f(i).isZ() && Math.abs(f(i)) < 8)
-			if (f(i)) {
-				X.push(i);
-				Y.push(f(i));
-			}
-	if (X.length < 2)
-		return;
-	let paint1 = function (ct) {
-		let h = 400;
-		let w = 400;
-		//Оси координат
-		ct.drawCoordinatePlane(w, h, {
-			hor: 1,
-			ver: 1
-		}, {
-			x1: '1',
-			y1: '1',
-			sh1: 13,
-		}, 20);
-		ct.scale(20, -20);
-		ct.lineWidth = 0.1;
-		//график
-		graph9AdrawFunction(ct, f, {
-			minX: -8.5,
-			maxX: 8.5,
-			minY: -8.8,
-			maxY: 8.5,
-			step: 0.05
+		function f(x) {
+			if (a > 0)
+				return Math.pow(a, x) + b;
+			return -Math.pow(a.abs(), x) + b;
+		}
+
+		let key = '509095';
+		let preference1 = ['functionOfX', 'valueX'];
+		let preference2 = ['withB', 'withoutB'];
+		let randFind = getSelectedPreferenceFromList(key, preference1);
+		let randB = getSelectedPreferenceFromList(key, preference2);
+
+		let a = slKrome([1], 0.25, 20, 0.25).pm();
+		let b = [sluchch(1, 10).pm(), 0][randB];
+		let chisl = sluchch(0, 5).pm();
+		genAssert(f(chisl).abs() >= 8);
+		genAssertAlmostInteger((chisl));
+		genAssertZ1000(f(chisl));
+		genAssert(f(chisl).abs() < 10000);
+
+		//слишком большое
+		let find, answ;
+		if (!randFind) {
+			find = `$f(${chisl.ts()})$`;
+			answ = f(chisl);
+		} else {
+			answ = chisl;
+			find = `значение $x$, при котором $f(x)=${f(chisl).ts()}$`;
+		}
+
+		let points = intPoints(f, {
+			minX: -8,
+			maxX: 8,
+			minY: -8,
+			maxY: 8
 		});
-		//точки
-		graph9AmarkCircles(ct, [X, Y].T(), 2, 0.15);
-	};
-	NAtask.setTask({
-		text: `На рисунке изображён график функции $f(x)=${` - `.esli(a < 0)}a^x+b$. Найдите ${find}. `,
-		answers: answ,
-		analys: `$f(x)=` + (a + `^{x}+` + (b)).replace('+0', '').plusminus() + `$`,
-	});
-	NAtask.modifiers.addCanvasIllustration({
-		width: 400,
-		height: 400,
-		paint: paint1,
-	});
-	return true;
-}, 1000000);
+
+		genAssert(points.length >= 2, 'Недостаточно точек');
+
+		let paint1 = function (ct) {
+			let h = 400;
+			let w = 400;
+			//Оси координат
+			ct.drawCoordinatePlane(w, h, {
+				hor: 1,
+				ver: 1
+			}, {
+				x1: '1',
+				y1: '1',
+				sh1: 13,
+			}, 20);
+			ct.scale(20, -20);
+			ct.lineWidth = 0.1;
+			//график
+			graph9AdrawFunction(ct, f, {
+				minX: -8.5,
+				maxX: 8.5,
+				minY: -8.8,
+				maxY: 8.5,
+				step: 0.05
+			});
+			//точки
+			graph9AmarkCircles(ct, points, 2, 0.15);
+		};
+		NAtask.setTask({
+			text: `На рисунке изображён график функции $f(x)=${` - `.esli(a < 0)}a^x${`+b`.esli(!randB)}$. Найдите ${find}. `,
+			answers: answ,
+			analys: `$f(x)=` + (a + `^{x}+` + (b)).replace('+0', '').plusminus() + `$`,
+			preference: [preference1, preference2],
+		});
+		NAtask.modifiers.allDecimalsToStandard(true);
+		NAtask.modifiers.addCanvasIllustration({
+			width: 400,
+			height: 400,
+			paint: paint1,
+		});
+	}, 100000);
+})();
 //509095 509089
