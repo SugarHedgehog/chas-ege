54802285f3451d9aec63731b276bbab66748b3f3
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-11-11 13:04:54 +0300

    misc_graph_2026

---
+++ zdn/misc_graph_2026/2/main.js
@@ -0,0 +1,6 @@
+window.nomer=[
+	509197,
+].iz()
+window.comment='509197 findAbscissa';
+window.nabor.preferences['509197'] = ['findAbscissa'];
+
---
+++ zdn/misc_graph_2026/7/main.js
@@ -0,0 +1,6 @@
+window.nomer=[
+	509271,
+].iz()
+window.comment='509271 findAbscissa withoutB withoutC';
+window.nabor.preferences['509271'] = ['findAbscissa','withoutB','withoutC'];
+
