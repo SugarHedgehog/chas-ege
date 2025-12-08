418f49e0e10aae269c92bb039db8c1f5c7b399d7
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-12-08 14:51:04 +0300

    nabor-override-d-1

---
+++ zdn/nabor/nabor-override-d-1.js
@@ -0,0 +1,59 @@
+console.log('Overriding...');
+// Здесь можно писать комментарии. Собственно, вот так!
+
+
+// Это у нас так номер варианта пишется!
+//$('#variantPrefix').val('1.');
+
+// Количество вариантов
+$('#cV').val('5');
+window.nabor.upak[1].main = function(){
+	window.nomer = 135;
+}
+
+window.nabor.upak[2].main = function(){
+	window.nomer = 4504;
+}
+
+window.nabor.upak[3].main = function(){
+	window.nomer = 27051;
+}
+
+window.nabor.upak[4].main = function(){
+	window.nomer = 3;
+}
+
+window.nabor.upak[5].main = function(){
+	window.nomer = 320175;
+	window.nabor.preferences['320175'] = [['two', 'three'].iz()];
+}
+
+window.nabor.upak[6].main = function(){
+	window.nomer = 4;
+}
+
+window.nabor.upak[7].main = function(){
+	window.nomer = 26850;
+}
+
+window.nabor.upak[8].main = function(){
+	window.nomer = 317541;
+}
+
+window.nabor.upak[9].main = function(){
+	window.nomer = 7;
+}
+
+window.nabor.upak[10].main = function(){
+	window.nomer = 5997;
+}
+
+window.nabor.upak[11].main = function(){
+	window.nomer = 508895;
+	window.nabor.preferences['508895'] = ['functionOfX'];
+}
+
+window.nabor.upak[12].main = function(){
+	window.nomer = 26700;
+}
+
