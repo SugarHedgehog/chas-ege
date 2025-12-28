8fdaeb5edacfb424fe202698ae01dcca4753915a
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-12-28 17:58:54 +0500

    [zdn] [fix] - 2069 - primary values

--- zdn/matege2023p/1/2069.js
+++ zdn/matege2023p/1/2069.js
@@ -4,12 +4,12 @@
 
 		let angle = sl(2, 44);
 		let condition = [
-			['больший', 45 + angle],
+			['больший острый', 45 + angle],
 			['меньший', 45 - angle]
 		].iz();
 
 		let paint1 = function(ctx) {
-			ctx.lineWidth = 2;
+			ctx.lineWidth = om.primaryLineWidth;
 			
 			let angle = Math.PI/2.9;
 			
