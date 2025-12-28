6e6b427683c65e2a4d23c1dd232e3c9b72eef60e
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-12-28 18:59:28 +0500

    [zdn] [pref] - 26724

--- zdn/matege2024p/12/26724.js
+++ zdn/matege2024p/12/26724.js
@@ -1,10 +1,19 @@
 (function() {
 	retryWhileError(function() {
 		'use strict';
+		let key = "26724";
+		let preference = ['maximum', 'minimum'];
+		let rand = getSelectedPreferenceFromList(key, preference);
+		let [forbidMinY, forbidMaxY] = [1 - rand, rand];
+
 		let a = sl(1,20).pm();
+
 		NAtask.setLocalExtremumTask({
 			expr: '(' + sl(1,10) + 'x^2 - ' + a + 'x +' + a + ')e^(x + ' + sl(-30,30) + ')' ,
 			authors: ['Николай Авдеев'],
+			forbidMinY,
+			forbidMaxY,
+			preference: preference,
 		});
 	}, 20);
 })();
