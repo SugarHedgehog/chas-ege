49d5028b277b21fe23a20be4d6c9bb8340dc71cb
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-12-28 18:59:28 +0500

    [zdn] [pref] - 26717

--- zdn/matege2024p/12/26717.js
+++ zdn/matege2024p/12/26717.js
@@ -1,24 +1,36 @@
 (function() {
 	retryWhileError(function() {
 		'use strict';
+		let key = "26717";
+		let preference1 = ['trivial', 'not_trivial'];
+		let preference2 = ['maximum', 'minimum'];
+		let rand1 = getSelectedPreferenceFromList(key, preference1);
+		let rand2 = getSelectedPreferenceFromList(key, preference2);
+		let [forbidMinY, forbidMaxY] = [1 - rand2, rand2];
 		let a = sl(2, 20);
 		let b = sl(1, 15);
-		let k = sl(-10, 10);
 		let c = a;
+		
+		let pow = [a + 'log(x+' + b + ')', 'log((x+' + b + ')^' + c + ')'][rand1];
+		let k = sl(-10, 10);
+		
 		if (sl1()) {
 			k = 0;
 			a = 1;
 		}
 		let l = -b - 1 - sl(0, 1.5, 0.5);
-		let arr1 = ['+', '-'];
-		arr1.shuffle();
+		let arr1 = ['+', '-'].shuffle();	
+
 		NAtask.setMinimaxFunctionTask({
-			expr: arr1[0] + [a + 'log(x+' + b + ')', 'log((x+' + b + ')^' + c + ')'].iz() + '+' + arr1[1] + c + 'x+' + k,
+			expr: arr1[0] + pow + '+' + arr1[1] + c + 'x+' + k,
 			leftEnd: '' + l,
 			rightEnd: '0',
 			primaryStep: 0.01,
 			secondaryStep: 0.001,
 			authors: ['Алендарь Сергей'],
+			forbidMinY,
+			forbidMaxY,
+			preference: [preference1, preference2],
 		});
 	}, 1000);
 })();
