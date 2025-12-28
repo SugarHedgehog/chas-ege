8ae82392e07d1792505677894daf105a814b2a0d
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-12-28 18:24:26 +0500

    [zdn] [ref] - 77341

--- zdn/mat2014/B2/31.js
+++ zdn/mat2014/B2/31.js
@@ -1,8 +1,9 @@
-(function(){'use strict';
-NAinfo.requireApiVersion(0, 0);
+(function () {
+	'use strict';
+	NAinfo.requireApiVersion(0, 0);
 
 
-// Чтобы не было бесконечных дробей в ответе
+	// Чтобы не было бесконечных дробей в ответе
 	do {
 		var students = sluchch(10, 50);
 		var percent = sluchch(20, 65, 15);
@@ -12,14 +13,14 @@ NAinfo.requireApiVersion(0, 0);
 	var school = sklonlxkand(['школа', 'лицей', 'гимназия'].iz());
 	var vuz = ['военных', 'специальных', 'частных'].iz();
 
-NAtask.setTask({
+	NAtask.setTask({
 
-	text: chislitlx(students,'выпускник') +' '+ school.re +' собираются учиться в '+ vuz +' вузах. ' +
-	'Они составляют '+ percent +'% от числа выпускников. Сколько в '+ school.pe +' выпускников?',
+		text: chislitlx(students, 'выпускник') + ' ' + school.re + ' собираются учиться в ' + vuz + ' вузах. ' +
+			'Они составляют ' + percent + '% от числа выпускников. Сколько в ' + school.pe + ' выпускников?',
 
-	answers: (students / percent * 100).ts(),
+		answers: (students / percent * 100).ts(),
 
-});
+	});
 })();
 //Обзад 77341
 //ProgerFF
