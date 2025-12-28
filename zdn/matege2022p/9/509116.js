7603f5ee0d6330c71d3146b215b7a24ad3b39c69
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-12-28 18:29:13 +0500

    [zdn] [fix] - 509116

--- zdn/matege2022p/9/509116.js
+++ zdn/matege2022p/9/509116.js
@@ -1,4 +1,4 @@
-retryWhileUndefined(function() {
+retryWhileUndefined(function () {
 	NAinfo.requireApiVersion(0, 2);
 
 	function f(x) {
@@ -34,7 +34,7 @@ retryWhileUndefined(function() {
 	});
 	if (p.length < 2)
 		return;
-	let paint1 = function(ct) {
+	let paint1 = function (ct) {
 		let h = 400;
 		let w = 600;
 		//Оси координат
