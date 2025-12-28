4690936e4245eefa8b70a3c5b52f000c9cb25dea
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-12-28 18:59:28 +0500

    [zdn] [pref] - 77455

--- zdn/matege2024p/12/77455.js
+++ zdn/matege2024p/12/77455.js
@@ -1,17 +1,25 @@
 (function() {
 	retryWhileError(function() {
 		'use strict';
-		let xsqrtx = ['x^(3/2)', 'x sqrt(x)'];
-		if (nabor.preferences && "77455" in nabor.preferences){
-			xsqrtx = [xsqrtx[nabor.preferences["77455"]]];
-		}
+		let key = "77455";
+		let preference1 = ['pow', 'sqrt'];
+		let preference2 = ['maximum', 'minimum'];
+		let rand1 = getSelectedPreferenceFromList(key, preference1);
+		let rand2 = getSelectedPreferenceFromList(key, preference2);
+
+		let xsqrtx = ['x^(3/2)', 'x sqrt(x)'][rand1];
+		let [forbidMinY, forbidMaxY] = [1 - rand2, rand2];
+
 		NAtask.setLocalExtremumTask({
 			expr: [
-				'' + sl(1,5) + xsqrtx.iz(),
-				sl(1,20) + 'x',
-				sl(0,100)
+				'' + sl(1, 5) + xsqrtx,
+				sl(1, 20) + 'x',
+				sl(0, 100)
 			].joinPlusMinus(),
 			authors: ['Николай Авдеев'],
+			forbidMinY,
+			forbidMaxY,
+			preference: [preference1, preference2],
 		});
 	}, 200);
 })();
