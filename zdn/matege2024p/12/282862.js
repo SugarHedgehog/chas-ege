ed2ea83b7d2edad1c0cef06ac4fd67bea4a84d4f
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-12-28 18:59:29 +0500

    [zdn] [pref] - 282862

--- zdn/matege2024p/12/282862.js
+++ zdn/matege2024p/12/282862.js
@@ -1,11 +1,17 @@
 (function() {
 	retryWhileError(function() {
 		'use strict';
+		let key = "282862";
+		let preference = ['maximum', 'minimum'];
+		let rand = getSelectedPreferenceFromList(key, preference);
+		let [forbidMinY, forbidMaxY] = [1 - rand, rand];
+
 		let a = sl(1, 30);
 		let b = slKrome(a, 1, 30);
 		let c = sl(-15, 15, 0.5);
 		let d = c + sl(3, 30, 0.5);
 		let arr = ['+', '-'];
+		
 		NAtask.setMinimaxFunctionTask({
 			expr: ['(x' + arr.iz() + a + ')^2','(' + ' x ' + arr.iz() + b + ')'].shuffle().join('') + arr.iz() + sl(1, 10),
 			leftEnd: '' + c,
@@ -13,6 +19,9 @@
 			primaryStep: 0.1,
 			secondaryStep: 0.001,
 			authors: ['Алендарь Сергей'],
+			forbidMinY,
+			forbidMaxY,
+			preference: preference,
 		});
 	}, 10000);
 })();
