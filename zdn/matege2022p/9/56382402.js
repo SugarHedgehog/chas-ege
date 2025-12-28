f92ae6dc6b697d3f3000337f1c05ecc22209062c
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-12-28 18:45:22 +0500

    [zdn] [fix] - 56382402

--- zdn/matege2022p/9/56382402.js
+++ zdn/matege2022p/9/56382402.js
@@ -1,4 +1,4 @@
-retryWhileUndefined(function() {
+retryWhileUndefined(function () {
 	NAinfo.requireApiVersion(0, 2);
 
 	function f(x) {
@@ -31,27 +31,27 @@ retryWhileUndefined(function() {
 
 	let question, answ;
 	switch (sl(1, 3)) {
-	case 1:
-		let num1 = sl(-10, 10);
-		question = 'значение дискриминанта уравнения $f(x)=' + num1 + '$';
-		answ = b.pow(2) - 4 * (c - num1) * a;
-		if ((answ * 1000).isZ())
+		case 1:
+			let num1 = sl(-10, 10);
+			question = 'значение дискриминанта уравнения $f(x)=' + num1 + '$';
+			answ = b.pow(2) - 4 * (c - num1) * a;
+			if ((answ * 1000).isZ())
+				break;
+		case 2:
+			let x = sl(6, 10).pm();
+			question = '$f(' + x + ')$';
+			answ = f(x);
 			break;
-	case 2:
-		let x = sl(6, 10).pm();
-		question = '$f(' + x + ')$';
-		answ = f(x);
-		break;
-	case 3:
-		let st = [
-			['произведение', a * b * c],
-			['сумму', a + b + c]
-		].iz();
-		answ = st[1];
-		question = st[0] + ' всех коэффициентов многочлена';
-		break;
+		case 3:
+			let st = [
+				['произведение', a * b * c],
+				['сумму', a + b + c]
+			].iz();
+			answ = st[1];
+			question = st[0] + ' всех коэффициентов многочлена';
+			break;
 	}
-	let paint1 = function(ct) {
+	let paint1 = function (ct) {
 		let h = 400;
 		let w = 400;
 		//Оси координат
