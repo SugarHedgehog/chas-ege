ad15361e0d5fbe1301ca219b0e167f0a8183ebb8
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-12-28 18:59:28 +0500

    [zdn] [pref] - 77427

--- zdn/matege2024p/12/77427.js
+++ zdn/matege2024p/12/77427.js
@@ -1,14 +1,24 @@
 (function() {
 	retryWhileError(function() {
 		'use strict';
+
+		let key = "77427";
+		let preference1 = ['linear', 'not_linear'];
+		let preference2 = ['maximum', 'minimum'];
+		let rand1 = getSelectedPreferenceFromList(key, preference1);
+		let rand2 = getSelectedPreferenceFromList(key, preference2);
+		let linear = [['+', '-'].iz() + sl(1, 100) + 'x ', ''][rand1];
+		let [forbidMinY, forbidMaxY] = [1 - rand2, rand2];
+
 		NAtask.setLocalExtremumTask({
 			expr:
-				// TODO: Do something with 1x
-				'' /*+ ['',sl(2,4).pm()].iz()*/ + 'x^3 ' +
-				['+','-'].iz() + sl(1,100) + 'x^2 ' +
-				['+','-'].iz() + sl(1,100) + 'x ' +
-				['+','-'].iz() + sl(1,1000),
+			// TODO: Do something with 1x
+				'' /*+ ['',sl(2,4).pm()].iz()*/ + 'x^3 ' + ['+', '-'].iz() + sl(1, 100) + 'x^2 ' + linear + ['+', '-'].iz() +
+				sl(1, 1000),
 			authors: ['Николай Авдеев'],
+			forbidMinY,
+			forbidMaxY,
+			preference: [preference1, preference2],
 		});
 	}, 2000);
 })();
