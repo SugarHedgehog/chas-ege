a20acae5ca814ff5155f55197ee107221ced0624
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-12-28 18:24:47 +0500

    [zdn] [ref] - 77343

--- zdn/mat2014/B2/32.js
+++ zdn/mat2014/B2/32.js
@@ -1,27 +1,28 @@
-(function(){'use strict';
-NAinfo.requireApiVersion(0, 0);
+(function () {
+	'use strict';
+	NAinfo.requireApiVersion(0, 0);
 
-	var goods=sklonlxkand(['тетрадь','шуруп','карандаш','ручка'].iz());//товар
-	var cost=sluchch(5,25);                                            //стоимость
-	var sum=sluchch(20,70);                                            //количество
-	var sumd=sluchch(30,50,10);                                        //количество,необходимое для скидки
-	var discount=sluchch(5,12);                                        // скидка
-	var client=['покупатель','школьник','ученик'].iz();                // покупатель
-	var build=['киоск','ларёк','магазин'].iz();                          // заведение
-	if (sumd<sum){
-		var m=(cost*sum*(1-discount/100)).ceil() ;   //чтобы лишний раз не пересчитывать
-	}else{
-		var m=(cost*sum).ceil();
+	var goods = sklonlxkand(['тетрадь', 'шуруп', 'карандаш', 'ручка'].iz());//товар
+	var cost = sluchch(5, 25);                                            //стоимость
+	var sum = sluchch(20, 70);                                            //количество
+	var sumd = sluchch(30, 50, 10);                                        //количество,необходимое для скидки
+	var discount = sluchch(5, 12);                                        // скидка
+	var client = ['покупатель', 'школьник', 'ученик'].iz();                // покупатель
+	var build = ['киоск', 'ларёк', 'магазин'].iz();                          // заведение
+	if (sumd < sum) {
+		var m = (cost * sum * (1 - discount / 100)).ceil();   //чтобы лишний раз не пересчитывать
+	} else {
+		var m = (cost * sum).ceil();
 	}
 
-NAtask.setTask({
+	NAtask.setTask({
 
-	text: goods.ie.toZagl()+' стоит '+chislitlx(cost,'рубль')+'. Сколько рублей заплатит '+
-	client+' за '+chislitlx(sum,goods)+', если при покупке больше '+chislitlx(sumd,goods)+
-	' '+build+' делает скидку '+discount+'% от стоимости всей покупки?',
+		text: goods.ie.toZagl() + ' стоит ' + chislitlx(cost, 'рубль') + '. Сколько рублей заплатит ' +
+			client + ' за ' + chislitlx(sum, goods) + ', если при покупке больше ' + chislitlx(sumd, goods) +
+			' ' + build + ' делает скидку ' + discount + '% от стоимости всей покупки?',
 
-	answers:  m,
-});
+		answers: m,
+	});
 })();
 // Обзад 77343
 // Павел Lopaste
