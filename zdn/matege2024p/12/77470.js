6e65b0d4cef840caa8577d52a5bb99009bf66191
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-12-28 18:59:28 +0500

    [zdn] [pref] - 77470

--- zdn/matege2024p/12/77470.js
+++ zdn/matege2024p/12/77470.js
@@ -1,16 +1,33 @@
 (function() {
 	retryWhileError(function() {
 		'use strict';
+		let key = "77470";
+		let preference1 = ['xfrac', 'fracx'];
+		let preference2 = ['maximum', 'minimum'];
+		let rand1 = getSelectedPreferenceFromList(key, preference1);
+		let rand2 = getSelectedPreferenceFromList(key, preference2);
+
 		let a = sl(2, 14);
-		let b = a * a;
+		let frac = ['x', '(x^2 + ' + a * a + ')'];
+		if(!rand1){
+			frac = frac.join('/');
+		} else {
+			frac = frac.reverse().join('/');
+		}
+		let [forbidMinY, forbidMaxY] = [1 - rand2, rand2];
+
 		let d = sl(-15, 15);
 		let e = d + sl(3, 30);
+
 		NAtask.setMinimaxFunctionTask({
-			expr: ['','-'].iz() + '((x^2+' + b + ')/x)',
+			expr: ['','-'].iz() + '('+ frac +')',
 			leftEnd: '' + d,
 			rightEnd: '' + e,
 			primaryStep: 0.1,
 			authors: ['Алендарь Сергей'],
+			forbidMinY,
+			forbidMaxY,
+			preference: [preference1, preference2],
 		});
 	}, 10000);
 })();
