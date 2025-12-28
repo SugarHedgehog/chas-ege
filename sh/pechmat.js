590e86ffca1d4402a01cd32d50d57814c757e03b
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-12-28 17:52:21 +0500

    [lib] [fix] - imageSize imageScale

--- sh/pechmat.js
+++ sh/pechmat.js
@@ -527,7 +527,7 @@ function replaceCanvasWithImgInTask(element, text) {
 	for (var i = 0; i < canvases.length; i++) {
 		var imageName = canvases[i].getAttribute('data-nonce').substr(3) + "n" + i;
 		preparedImages[imageName] = canvases[i].toDataURL().replace('data:image/png;base64,','');
-		text = text.replace(/<canvas.*?<\/canvas>/, '\\addpictoright[0.4\\linewidth]{'+imageName+'}');
+		text = text.replace(/<canvas.*?<\/canvas>/, '\\addpictoright[\\imageSize][\\imageScale]{'+imageName+'}');
 	}
 	if (canvases.length) {
 		text =
