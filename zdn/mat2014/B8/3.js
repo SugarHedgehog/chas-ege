47c24a5c480d8bd205874c9c22f5daaaba63c754
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-12-28 17:53:00 +0500

    [zdn] [fix] - 3 - primary values

--- zdn/mat2014/B8/3.js
+++ zdn/mat2014/B8/3.js
@@ -11,7 +11,7 @@
 		var b = c * c - a * a;
 
 		let paint1 = function(ctx) {
-			ctx.lineWidth = 2;
+			ctx.lineWidth = om.primaryLineWidth;
 			ctx.strokeStyle = om.secondaryBrandColors.iz();
 
 			ctx.drawLine(10, 370, 390, 370);
@@ -19,8 +19,8 @@
 			ctx.drawLine(10, 50, 390, 370);
 
 			//прямой угол
-			ctx.lineWidth = 1.2;
-			ctx.strokeStyle = ["#D777F2","#F2A2D6"].iz();
+			ctx.lineWidth = om.secondaryLineWidth;
+			ctx.strokeStyle = om.primaryBrandColors.iz();
 
 			ctx.drawLine(10, 370 - 20, 10 + 20, 370 - 20);
 			ctx.drawLine(10 + 20, 370, 10 + 20, 370 - 20);
