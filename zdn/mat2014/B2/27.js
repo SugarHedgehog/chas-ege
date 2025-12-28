9f15bb81a6352bed17a051e293190b52db97cd2f
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-12-28 18:24:16 +0500

    [zdn] [ref] - 318582

--- zdn/mat2014/B2/27.js
+++ zdn/mat2014/B2/27.js
@@ -1,36 +1,36 @@
-(function(){'use strict';
-NAinfo.requireApiVersion(0, 0);
+(function () {
+	'use strict';
+	NAinfo.requireApiVersion(0, 0);
 
 	var G_IN_POUND = 400;
 	var people_in_recipe;
 	var filling_in_recipe_factor = sl(1, 3) * 10;
-	var filling_in_recipe_factor_a = sl(1, filling_in_recipe_factor-1);
+	var filling_in_recipe_factor_a = sl(1, filling_in_recipe_factor - 1);
 	var required_people;
 
 	var answer;
 
-	do
-	{
-	people_in_recipe = sl(5, 30);
-	required_people = slKrome(people_in_recipe, 3, 12);
-	
-	answer = G_IN_POUND / filling_in_recipe_factor * filling_in_recipe_factor_a / people_in_recipe * required_people;
+	do {
+		people_in_recipe = sl(5, 30);
+		required_people = slKrome(people_in_recipe, 3, 12);
+
+		answer = G_IN_POUND / filling_in_recipe_factor * filling_in_recipe_factor_a / people_in_recipe * required_people;
 	}
 
-	while(!(answer * 100).isZ());
+	while (!(answer * 100).isZ());
 
-	var product = sklonlxkand([ 'чернослив', 'черника', 'земляника', 'ежевика' ].iz());
+	var product = sklonlxkand(['чернослив', 'черника', 'земляника', 'ежевика'].iz());
 
-NAtask.setTask({
+	NAtask.setTask({
 
-	text: 'Имеется рецепт пирога с ' + product.te + '. Для пирога на ' + people_in_recipe + ' человек следует взять ' +
-                  '$'+filling_in_recipe_factor_a.frac(filling_in_recipe_factor) + '$ фунта ' + product.re +
-                  '. Сколько граммов ' + product.re + ' следует взять для пирога, рассчитанного на ' + required_people +
-                  ' человек? Считайте, что 1 фунт равен 0,4 кг',
+		text: 'Имеется рецепт пирога с ' + product.te + '. Для пирога на ' + people_in_recipe + ' человек следует взять ' +
+			'$' + filling_in_recipe_factor_a.frac(filling_in_recipe_factor) + '$ фунта ' + product.re +
+			'. Сколько граммов ' + product.re + ' следует взять для пирога, рассчитанного на ' + required_people +
+			' человек? Считайте, что 1 фунт равен 0,4 кг',
 
-	answers: answer,
+		answers: answer,
 
-});
+	});
 })();
 //Обзад 318582
 // Sergey Naumov aka DrMGC
