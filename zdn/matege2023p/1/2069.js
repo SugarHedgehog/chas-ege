48017ad13c48060d4252df2bb0811c1effad9b35
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2026-01-08 15:11:10 +0300

    [zdn] [pref] - 2069.js

--- zdn/matege2023p/1/2069.js
+++ zdn/matege2023p/1/2069.js
@@ -1,6 +1,9 @@
-(function() {
-	retryWhileError(function() {
+(function () {
+	retryWhileError(function () {
 		NAinfo.requireApiVersion(0, 2);
+		let key = '2069';
+		let preference = ['angleInTriangle', 'angleBetweenMedianAndBbisector'];
+		let rand = getSelectedPreferenceFromList(key, preference);
 
 		let angle = sl(2, 44);
 		let condition = [
@@ -8,37 +11,45 @@
 			['меньший', 45 - angle]
 		].iz();
 
-		let paint1 = function(ctx) {
+		let paint1 = function (ctx) {
 			ctx.lineWidth = om.primaryLineWidth;
-			
-			let angle = Math.PI/2.9;
-			
+
+			let angle = Math.PI / 2.9;
+
 			ctx.strokeStyle = om.secondaryBrandColors.iz();
-			ctx.drawLine(10, 250, 390-8, 250);
-			let ver = ctx.drawLineAtAngle(10, 250, -angle, 200-25);
-			ctx.drawLineAtAngle(ver.x, ver.y, -angle+Math.PI/2, 350-20);
+			ctx.drawLine(10, 250, 390 - 8, 250);
+			let ver = ctx.drawLineAtAngle(10, 250, -angle, 200 - 25);
+			ctx.drawLineAtAngle(ver.x, ver.y, -angle + Math.PI / 2, 350 - 20);
 			//штрихи
-			ctx.strokeInMiddleOfSegment(10, 250, (390-8)/2, 250, 10);
-			ctx.strokeInMiddleOfSegment(10, 250, 3*(390-8)/2, 250, 10);
+			ctx.strokeInMiddleOfSegment(10, 250, (390 - 8) / 2, 250, 10);
+			ctx.strokeInMiddleOfSegment(10, 250, 3 * (390 - 8) / 2, 250, 10);
 			//биссектриса
-			ctx.strokeStyle = om.primaryBrandColors [0];
-			let bis = ctx.drawLineAtAngle(ver.x, ver.y, (-angle+Math.PI/2)+Math.PI/4, 160+2);
+			ctx.strokeStyle = om.primaryBrandColors[0];
+			let bis = ctx.drawLineAtAngle(ver.x, ver.y, (-angle + Math.PI / 2) + Math.PI / 4, 160 + 2);
 			//медиана
-			ctx.strokeStyle = om.primaryBrandColors [1];
-			ctx.drawLine(ver.x, ver.y,(390-8)/2, 250);
-			
-			ctx.strokeStyle = om.primaryBrandColors [0];
-			ctx.arcBetweenSegments([10, 250,ver.x, ver.y, bis.x, bis.y], 30);
-			ctx.arcBetweenSegments([390-8, 250,ver.x, ver.y, bis.x, bis.y], 38);
+			ctx.strokeStyle = om.primaryBrandColors[1];
+			ctx.drawLine(ver.x, ver.y, (390 - 8) / 2, 250);
+
+			ctx.strokeStyle = om.primaryBrandColors[0];
+			ctx.arcBetweenSegments([10, 250, ver.x, ver.y, bis.x, bis.y], 30);
+			ctx.arcBetweenSegments([390 - 8, 250, ver.x, ver.y, bis.x, bis.y], 38);
 
 		};
 
 		NAtask.setTask({
-			text: 'Угол между биссектрисой и медианой прямоугольного треугольника, ' +
-				'проведёнными из вершины прямого угла, равен $' + angle + '^{\\circ}$. ' +
-				'Найдите ' + condition[0] + ' угол прямоугольного треугольника. Ответ дайте в градусах.',
-			answers: condition[1],
+			questions: [[
+				{
+					text: `Угол между биссектрисой и медианой прямоугольного треугольника, проведёнными из вершины прямого угла, равен $${angle}^{\\circ}$. Найдите ${condition[0]} угол прямоугольного треугольника.`,
+					answers: condition[1],
+				},
+				{
+					text: `${condition[0].toZagl()} угол прямоугольного треугольника равен $${condition[1].ts()}^{\\circ}$. Найдите величину угла между биссектрисой и медианой, проведёнными из вершины прямого угла.`,
+					answers: angle,
+				},
+			][rand]],
+			postquestion: ' Ответ дайте в градусах.',
 			analys: '',
+			preference: preference,
 		});
 		NAtask.modifiers.addCanvasIllustration({
 			width: 400,
