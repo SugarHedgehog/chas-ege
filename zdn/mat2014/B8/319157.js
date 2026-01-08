cbcf4680a71b310c6294b360509c3871807f8d27
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2026-01-08 15:05:18 +0300

    [zdn] [pref] - 319057.js

--- zdn/mat2014/B8/319157.js
+++ zdn/mat2014/B8/319157.js
@@ -1,46 +1,72 @@
-(function() {
-	'use strict';
-	NAinfo.requireApiVersion(0, 0);
-	let side = 'M';
-	let lowSide = 'AD';
-	let middle = ['AB', 'CD', 'BC', 'AD'].iz();
-	let answer = sluchch(5, 69);
-	let S = 4 * answer;
-	if (middle == 'BC' || middle == 'AD') {
-		S = 2 * answer;
-		if (middle == 'AD')
-			lowSide = 'BC';
-	}
-	let paint1 = function(ctx) {
-		ctx.lineWidth = om.primaryLineWidth;
-		ctx.strokeStyle  = om.secondaryBrandColors.iz();
+(function () {
+	retryWhileError(function () {
+		NAinfo.requireApiVersion(0, 2);
+		'use strict';
+		let key = '319157';
+		let preference = ['areaOfTriangle', 'areaOfTrapezoid'];
+		let rand = getSelectedPreferenceFromList(key, preference);
 
-		ctx.drawLine(100, 80, 350, 80);
-		ctx.drawLine(10, 320, 270, 320);
-		ctx.drawLine(100, 80, 10, 320);
-		ctx.drawLine(270, 320, 350, 80);
-		
-		ctx.strokeStyle = om.primaryBrandColors.iz();
+		let letters = latbukv.slice(0, 4).concat('M');
+		let randMimus = sl1();
 
-		ctx.drawLine(10, 320, 310, 200);
+		let par = new Parallelogram({
+			lengths: {
+				lengthAB: sl(5, 10),
+				lengthBC: sl(5, 10)
+			},
+			angles: {
+				angle: {
+					angleA: Math.PI / (6) * [-1, 1][randMimus],
+				},
+			},
+		});
+		genAssertAlmostInteger(par.area());
 
-		ctx.lineWidth = om.secondaryLineWidth;
-		ctx.drawLine(300 + 15, 150, 320 + 10, 160);
-		ctx.drawLine(270 + 15, 250, 290 + 10, 260);
+		par.addVertexToConnectionMatrix(par.segmentAB.middle(), 'E');
+		par.connectVerticesInConnectionMatrix([3, 4]);
+		let points = autoScale(par.vertices);
 
-	};
+		let paint1 = function (ctx) {
+			let h = 400;
+			let w = 400;
 
-	NAtask.setTask({
-		text: 'Площадь параллелограмма $ABCD$ равна ' + S + '. Точка $' + side + '$ – середина стороны $' + middle +
-			'$. Найдите площадь треугольника $' + lowSide + side + '$.',
-		answers: answer,
-	});
-	NAtask.modifiers.variativeABC(); //расставляем случайные буквы
-	NAtask.modifiers.addCanvasIllustration({
-		width: 400,
-		height: 400,
-		paint: paint1,
-	});
+			ctx.translate(w / 2, h / 2);
+
+			ctx.scale(1, -1);
+			ctx.strokeStyle = om.secondaryBrandColors;
+
+			ctx.lineWidth = 2;
+			ctx.drawFigure(points, par.connectionMatrix);
+			ctx.strokeStyle = om.primaryBrandColors.iz();
+			ctx.strokeInMiddleOfSegment(points[0].x, points[0].y, points[4].x, points[4].y, 7, 1);
+			ctx.strokeInMiddleOfSegment(points[1].x, points[1].y, points[4].x, points[4].y, 7, 1);
+
+			ctx.scale(1, -1);
+			ctx.font = "20px liberation_sans";
+			points.forEach((elem, i) => ctx.fillText(letters[i], elem.x, -elem.y + ((i < 2 || i > 3) && randMimus ? 25 : -5)));
+		};
+
+		NAtask.setTask({
+			text: 'Площадь параллелограмма $ABCD$ равна $' + par.area().ts() +
+				'$. Точка $M$ – середина стороны $AB$. Найдите площадь ',
+			questions: [[{
+				text: 'треугольника $ABM$.',
+				answers: par.area() * 0.25,
+			},
+			{
+				text: 'трапеции $BCDM$.',
+				answers: par.area() * 0.75,
+			}][rand]],
+			preference: preference,
+		});
+		NAtask.modifiers.variativeABC(letters); //расставляем случайные буквы
+		NAtask.modifiers.addCanvasIllustration({
+			width: 400,
+			height: 400,
+			paint: paint1,
+		});
+	}, 1000);
 })();
-//Обр.задания 319157
+//https://ege.sdamgia.ru/problem?id=319057
+//https://oge.sdamgia.ru/problem?id=349612
 //Антипова Татьяна
