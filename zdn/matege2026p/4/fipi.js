a9de1ca89f8b823a7b0689f8b85e83c70c929241
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2026-01-16 17:07:29 +0300

    Merge branch 'matege2026p'

---
+++ zdn/matege2026p/4/fipi.js
@@ -0,0 +1,34 @@
+window.nomer=[
+	1,
+	4,
+	5,
+	7,
+	9,
+	282853,
+	320169,
+	320181,
+	320183,
+	320191,
+	320194,
+].iz()
+window.comment='Начала теории вероятностей';
+if (!window.nabor)
+	window.nabor = {};
+if (!window.nabor.preferences)
+	window.nabor.preferences = {};
+window.nabor.preferences['5'] = ['first'];
+window.nabor.preferences['5'] = ['last'];
+window.nabor.preferences['320169'] = ['girl'];
+window.nabor.preferences['320169'] = ['boy'];
+window.nabor.preferences['320191'] = ['inAuditorium'];
+window.nabor.preferences['320191'] = ['notInAuditorium'];
+window.nabor.preferences['1'] = ['two_tosses'];
+window.nabor.preferences['1'] = ['three_tosses'];
+window.nabor.preferences['320183'] = ['three_games','equal'];
+window.nabor.preferences['320183'] = ['three_games','more'];
+window.nabor.preferences['320183'] = ['three_games','less'];
+window.nabor.preferences['282853'] = ['two_times'];
+window.nabor.preferences['4'] = ['notHaveDefectInQuest','haveDefect'];
+window.nabor.preferences['4'] = ['haveDefectInQuest','haveDefect'];
+window.nabor.preferences['4'] = ['notHaveDefectInQuest','notHaveDefect'];
+window.nabor.preferences['4'] = ['haveDefectInQuest','notHaveDefect'];
---
+++ zdn/matege2026p/4/main.js
@@ -0,0 +1,40 @@
+window.nomer=[
+	1,
+	4,
+	5,
+	7,
+	9,
+	282853,
+	320169,
+	320181,
+	320183,
+	320191,
+	320194,
+].iz()
+window.comment='Начала теории вероятностей';
+
+// fast_set_year.sh metadata
+/* array entries:
+"320181"
+"320194"
+"5 первые_участницы first"
+"7"
+"5 последня_участница last"
+"320169 девочка girl"
+"320169 мальчик boy"
+"9"
+"320191 inAuditorium inAuditorium"
+"320191 notInAuditorium notInAuditorium"
+"1 two_tosses two_tosses"
+"1 three_tosses three_tosses"
+"320183 три_матча_ровно three_games equal"
+"320183 три_матча_больше three_games more"
+"320183 три_матча_меньше three_games less"
+"282853 два_броска two_times"
+"4 notHaveDefectInQuest_haveDefect notHaveDefectInQuest haveDefect"
+"4 haveDefectInQuest_haveDefect haveDefectInQuest haveDefect"
+"4 notHaveDefectInQuest_notHaveDefect notHaveDefectInQuest notHaveDefect"
+"4 haveDefectInQuest_notHaveDefect haveDefectInQuest notHaveDefect"
+ */
+// forbidOpenEnds answer: 
+// forbidDecimalFractions answer: 
