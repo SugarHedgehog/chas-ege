2ad1365dbbbba9719abddf28c6d77edd52a90e70
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2026-01-16 16:47:03 +0300

    matege2026p - 8

---
+++ zdn/matege2026p/8/fipi.js
@@ -0,0 +1,30 @@
+window.nomer=[
+	2542,
+	27492,
+	27494,
+	27502,
+	27505,
+	317541,
+	317544,
+	500248,
+	562751,
+].iz()
+window.comment='Производная и первообразная';
+if (!window.nabor)
+	window.nabor = {};
+if (!window.nabor.preferences)
+	window.nabor.preferences = {};
+window.nabor.preferences['27505'] = ['kMoreZero'];
+window.nabor.preferences['27505'] = ['kLessZero'];
+window.nabor.preferences['500248'] = ['derivative_is_positive','symbol'];
+window.nabor.preferences['500248'] = ['derivative_is_negative','symbol'];
+window.nabor.preferences['2542'] = ['number'];
+window.nabor.preferences['317544'] = ['derivative_is_smallest','number'];
+window.nabor.preferences['317544'] = ['derivative_is_largest','number'];
+window.nabor.preferences['27494'] = ['maximum_points_on_the_segment','number'];
+window.nabor.preferences['27494'] = ['minimum_points_on_the_segment','number'];
+window.nabor.preferences['27492'] = ['largest_value'];
+window.nabor.preferences['27492'] = ['smallest_value'];
+window.nabor.preferences['317541'] = ['function_is_increasing'];
+window.nabor.preferences['317541'] = ['function_is_decreasing'];
+window.nabor.preferences['27502'] = ['extreme_point_on_the_segment'];
