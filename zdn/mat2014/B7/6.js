9d424996859d8f013bc3715ed44eb06a329e803e
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2026-01-22 16:09:06 +0300

    [zdn] [pref] - 6

--- zdn/mat2014/B7/6.js
+++ zdn/mat2014/B7/6.js
@@ -2,13 +2,13 @@
 	'use strict';
 	
 	let key = '6';
-	let preference1 = ['2', '3', '4', '5'];
-	let powerRand = getSelectedPreferenceFromList(key, preference1);		 
-	let preference2 = ['linearPart', 'denominatorPart', 'numeratorPart'];
-	let partRand = getSelectedPreferenceFromList(key, preference2);		 
+	let preference1 = ['power_2', 'power_3', 'power_4', 'power_5'];
+	let rand1 = getSelectedPreferenceFromList(key, preference1);
+	let preference2 = ['no_frac', 'inverse_frac', 'frac'];
+	let rand2 = getSelectedPreferenceFromList(key, preference2);
 
 	var b = sluchch(1, 9).pm();
-	var power = Number(preference1[powerRand]);
+	var power = rand1 + 2;
 	var c = sluchch(1, 9);
 	if (power % 2) {
 		c *= [1, -1].iz();
@@ -22,17 +22,19 @@
 
 	var parts = [
 		['\\sqrt' + textpower + '{' + [a, b + 'x'].slag0() + '}', c],
-		['\\sqrt' + textpower + '{\\frac{' + numerator.pow(power) * multiplier + '}{' + [('' + a * multiplier).esli(a *	multiplier), b * multiplier + 'x'].slag() + '}}', numerator.texrndfrac(c)],
-		['\\sqrt' + textpower + '{\\frac{' + [('' + a * multiplier).esli(a * multiplier), b * multiplier + 'x'].slag() + '}{' + numerator.pow(power) * multiplier + '}}', c.texrndfrac(numerator)
+		['\\sqrt' + textpower + '{\\frac{' + numerator.pow(power) * multiplier + '}{' + [('' + a * multiplier).esli(a *
+			multiplier), b * multiplier + 'x'].slag() + '}}', numerator.texrndfrac(c)],
+		['\\sqrt' + textpower + '{\\frac{' + [('' + a * multiplier).esli(a * multiplier), b * multiplier + 'x'].slag() +
+			'}{' + numerator.pow(power) * multiplier + '}}', c.texrndfrac(numerator)
 		],
-	][partRand];
+	][rand2];
 
 
 	chas2.task.setEquationTask({
 		parts: parts,
 		roots: x,
 		enablePartsSubtraction: 1,
-		preference: [preference1, [preference2]],
+		preference: [preference1, preference2],
 	});
 })();
 // В том числе РешуЕГЭ 501205
