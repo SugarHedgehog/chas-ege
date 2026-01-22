8e5729d73fb17844d316cdad261a884d1a6c27eb
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2026-01-22 15:15:14 +0300

    matege2026p - 2

---
+++ zdn/matege2026p/2/main.js
@@ -0,0 +1,21 @@
+window.nomer=[
+	4504,
+	644849,
+	649891,
+	660709,
+	64485008,
+	649891001,
+].iz()
+window.comment='Вектора';
+
+// fast_set_year.sh metadata
+/* array entries:
+"649891"
+"4504"
+"644849"
+"64485008"
+"649891001"
+"660709"
+ */
+// forbidOpenEnds answer: 
+// forbidDecimalFractions answer: 
