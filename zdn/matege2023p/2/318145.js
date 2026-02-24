8057643662f691d70f67ea679e78342920bbd21f
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2026-02-24 14:40:11 +0500

    [zdn] [fix] - 318145

--- zdn/matege2023p/2/318145.js
+++ zdn/matege2023p/2/318145.js
@@ -70,7 +70,7 @@
 		NAinfo.requireApiVersion(0, 2);
 		NAtask.setTask({
 			text: 'В сосуде, имеющем форму конуса, уровень жидкости достигает ' +
-				'$' + numerator.texfrac(denominator) + '$ высоты. Объём жидкости равен ' + volumeDown + 'мл. ' +
+				'$' + numerator.texfrac(denominator) + '$ высоты. Объём жидкости равен ' + volumeDown + ' мл. ' +
 				'Сколько миллилитров жидкости ' + question[0] + '?',
 			answers: question[1],
 			authors: ['Суматохина Александра'],
