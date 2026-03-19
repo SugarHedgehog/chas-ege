7ba909fe62a912ca752615fd678afc5b6ba6d676
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2026-03-19 14:19:04 +0300

    nabor-override-all

---
+++ zdn/nabor/nabor-override-all.js
@@ -0,0 +1,43 @@
+console.log('Overriding...');
+
+// Выставляем экспорт в LaTeX
+$('#prepareLaTeX').prop("checked", true);
+
+// Редактируемый текст 
+$('#redakt').prop("checked", true);
+//Увеличить шрифт 
+$('#largeFont').prop("checked", false);
+//Не разрывать страницу между вариантами 
+$('#nopagebreak').prop("checked", false);
+//Компактные таблицы ответов 
+$('#compact-answers').prop("checked", true);
+//$('#solutions-into-answers').prop("checked", false);
+$('#solutions-into-answers').prop("checked", false);
+//Разделять таблицы ответов при достижении количества заданий:
+$('#splitAnswerTables').prop("checked", true);
+// количество заданий
+$('#split-answers-number').val(30);
+// Не преобразовывать графики в фоновые изображения
+$('#nobackground').prop("checked", false);
+// Начать нумерацию вариантов с
+$('#customNumber').prop("checked", true);
+$('#start-number').val(1)
+// Не указывать данные вариантов
+$('#vanishVariants').prop("checked", false);
+// Генерировать преимущественно натуральные числа
+$('#forceIntegers').prop("checked", true);
+// Генерировать ТОЛЬКО целые числа
+$('#onlyIntegers').prop("checked", true);
+// Требовать уникальных ответов, решений или сигнатур (осторожно, ответов может не хватить!) 
+$('#uniqueAnswersAndSolutions').prop("checked", false);
+// Только в пределах одного варианта 
+$('#uniqueAnswersOnlyInOneVariant').prop("checked", false);
+// Сквозная нумерация заданий, начиная с
+$('#transitTaskNumbers').prop("checked", false);
+$('#start-transit-number').val(1);
+// Начинать нумерацию однотипных заданий с 
+$('#first-task-number').val(1);
+// Возможность выгрузки в LaTeX
+$('#prepareLaTeX').prop("checked", true);
+// Зерно ГПСЧ
+//$('#randomSeed').val(Date.now());
