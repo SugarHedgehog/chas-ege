d6ff3577963390fd5d7a4f1157f61a2a4247b411
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2026-03-19 14:19:04 +0300

    nabor-override-d-7

---
+++ zdn/nabor/nabor-override-7.js
@@ -0,0 +1,57 @@
+console.log('Overriding...');
+// Здесь можно писать комментарии. Собственно, вот так!
+
+
+// Это у нас так номер варианта пишется!
+$('#variantPrefix').val('7.');
+
+// Количество вариантов
+$('#cV').val('5');
+window.nabor.upak[1].main = function(){
+	window.nomer = 119;
+}
+
+window.nabor.upak[2].main = function(){
+	window.nomer = 4504;
+}
+
+window.nabor.upak[3].main = function(){
+	window.nomer = 245341;
+}
+
+window.nabor.upak[4].main = function(){
+	window.nomer = 5;
+}
+
+window.nabor.upak[5].main = function(){
+	window.nomer = 201;
+}
+
+window.nabor.upak[6].main = function(){
+	window.nomer = 26648;
+}
+
+window.nabor.upak[7].main = function(){
+	window.nomer = 4;
+}
+
+window.nabor.upak[8].main = function(){
+	window.nomer = 2749202;
+}
+
+window.nabor.upak[9].main = function(){
+	window.nomer = 1;
+}
+
+window.nabor.upak[10].main = function(){
+	window.nomer = 26588;
+}
+
+window.nabor.upak[11].main = function(){
+	window.nomer = 508951;
+}
+
+window.nabor.upak[12].main = function(){
+	window.nomer = 32;
+}
+
