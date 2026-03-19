662b3575e05a3656039b3e8f12c84d049cdf1fc7
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2026-03-19 14:19:03 +0300

    nabor-override-d-5

---
+++ zdn/nabor/nabor-override-d-5.js
@@ -0,0 +1,62 @@
+console.log('Overriding...');
+// Здесь можно писать комментарии. Собственно, вот так!
+
+
+// Это у нас так номер варианта пишется!
+//$('#variantPrefix').val('1.');
+
+// Количество вариантов
+$('#cV').val('5');
+window.nabor.upak[1].main = function(){
+	window.nomer = 92;
+}
+
+window.nabor.upak[2].main = function(){
+	window.nomer = 64485008;
+}
+
+window.nabor.upak[3].main = function(){
+	window.nomer = 27118;
+}
+
+window.nabor.upak[4].main = function(){
+	window.nomer = 4;
+}
+
+window.nabor.upak[5].main = function(){
+	window.nomer = 320203;
+}
+
+window.nabor.upak[6].main = function(){
+	window.nomer = 6;
+	window.nabor.preferences['6'] = ['2', 'linearPart'];
+}
+
+window.nabor.upak[7].main = function(){
+	window.nomer = 26856;
+}
+
+window.nabor.upak[8].main = function(){
+	window.nomer = 2542;
+	window.nabor.preferences['2542'] = ['number'];
+}
+
+window.nabor.upak[9].main = function(){
+	window.nomer = 9;
+}
+
+window.nabor.upak[10].main = function(){
+	window.nomer = 14;
+	chas2.task.setTask.forbidDecimalFractions = true;
+}
+
+window.nabor.upak[11].main = function(){
+	window.nomer = 509095;
+	window.nabor.preferences['509095'] = ['functionOfX', 'withoutB'];
+	chas2.task.setTask.forbidDecimalFractions = false;
+}
+
+window.nabor.upak[12].main = function(){
+	window.nomer = 31;
+}
+
