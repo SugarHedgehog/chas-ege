b21a869be7edc50d86d3ed4dce586ab5da83e96f
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2026-03-19 14:19:05 +0300

    nabor-override-d-8

---
+++ zdn/nabor/nabor-override-8.js
@@ -0,0 +1,59 @@
+console.log('Overriding...');
+// Здесь можно писать комментарии. Собственно, вот так!
+
+
+// Это у нас так номер варианта пишется!
+$('#variantPrefix').val('8.');
+
+// Количество вариантов
+$('#cV').val('5');
+window.nabor.upak[1].main = function(){
+	window.nomer = 177;
+}
+
+window.nabor.upak[2].main = function(){
+	window.nomer = 644849;
+}
+
+window.nabor.upak[3].main = function(){
+	window.nomer = 245351;
+	window.nabor.preferences['245351'] = ['questionSphere', 'volumeCone', 'volumeSphere'];
+}
+
+window.nabor.upak[4].main = function(){
+	window.nomer = 7;
+}
+
+window.nabor.upak[5].main = function(){
+	window.nomer = 320172;
+}
+
+window.nabor.upak[6].main = function(){
+	window.nomer = 26650;
+}
+
+window.nabor.upak[7].main = function(){
+	window.nomer = 14;
+}
+
+window.nabor.upak[8].main = function(){
+	window.nomer = 2750201;
+}
+
+window.nabor.upak[9].main = function(){
+	window.nomer = 12;
+}
+
+window.nabor.upak[10].main = function(){
+	window.nomer = 26597;
+}
+
+window.nabor.upak[11].main = function(){
+	window.nomer = 509271;
+	window.nabor.preferences['509271'] = ['findAbscissa','withoutB','withoutC'];
+}
+
+window.nabor.upak[12].main = function(){
+	window.nomer = 77455;
+}
+
