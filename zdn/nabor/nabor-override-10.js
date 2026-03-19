29c117d77b0f031c94a83315c875f84cba0bac45
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2026-03-19 14:19:05 +0300

    nabor-override-d-10

---
+++ zdn/nabor/nabor-override-10.js
@@ -0,0 +1,63 @@
+console.log('Overriding...');
+// Здесь можно писать комментарии. Собственно, вот так!
+
+
+// Это у нас так номер варианта пишется!
+$('#variantPrefix').val('10.');
+ 
+// Количество вариантов
+$('#cV').val('5');
+window.nabor.upak[1].main = function(){
+	window.nomer = 319157;
+	window.nabor.preferences['319157'] = ['areaOfTrapezoid'];
+}
+
+window.nabor.upak[2].main = function(){
+	window.nomer = 4504;
+}
+
+window.nabor.upak[3].main = function(){
+	window.nomer = 27051;
+}
+
+window.nabor.upak[4].main = function(){
+	window.nomer = 320183;
+}
+
+window.nabor.upak[5].main = function(){
+	window.nomer = 320175;
+	window.nabor.preferences['320175'] = ['three'];
+}
+
+window.nabor.upak[6].main = function(){
+	window.nomer = 26671;
+}
+
+window.nabor.upak[7].main = function(){
+	window.nomer = 11;
+	window.nabor.preferences['11'] = ['integer_double_angle'];
+}
+
+window.nabor.upak[8].main = function(){
+	window.nomer = 27494;
+	window.nabor.preferences['27494'] = ['minimum_points_on_the_segment_number'];
+}
+
+window.nabor.upak[9].main = function(){
+	window.nomer = 27975;
+}
+
+window.nabor.upak[10].main = function(){
+	window.nomer = 99615;
+}
+
+window.nabor.upak[11].main = function(){
+	window.nomer = 509149;
+	window.nabor.preferences['509149'] = ['findAbscissa','simpleLinearFunc'];
+}
+
+window.nabor.upak[12].main = function(){
+	window.nomer = 26713;
+	window.nabor.preferences['509149'] = ['negative_pow','minimum'];
+}
+
