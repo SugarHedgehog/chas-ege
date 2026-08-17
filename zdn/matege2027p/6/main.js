649ce35cc231ad1a196015623bc3112c3f0841cc
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2026-08-17 16:45:23 +0300

    [zdn] [new] - 509353

---
+++ zdn/matege2027p/6/main.js
@@ -0,0 +1,21 @@
+window.nomer=[
+	509353
+].iz()
+window.comment='Статистика и вероятность';
+
+// fast_set_year.sh metadata
+/* array entries:
+"77384"
+"282849"
+"6 power_2_no_frac power_2 no_frac"
+"6 power_3_no_frac power_3 no_frac"
+"26671"
+"5 no_frac no_frac"
+"5 frac frac"
+"26650"
+"3"
+"2"
+"26658"
+ */
+// forbidOpenEnds answer: 
+// forbidDecimalFractions answer: 
